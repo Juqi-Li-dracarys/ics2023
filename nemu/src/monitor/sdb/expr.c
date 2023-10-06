3b1077208a08d7ea327d430f3c397ebd65592185
tracer-ics2023 <tracer@njuics.org>
2023-10-06 23:59:28 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:59:28 up 51 min,  1 user,  load average: 0.85, 0.78, 0.68

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -229,6 +229,6 @@ word_t expr(char *e, bool *success) {
   for(int i = 0; i <= nr_token; i++){
     printf("%d ----- %s\n", tokens[i].type, tokens[i].str);
   }
-
+  *success = true;
   return 0;
 }
