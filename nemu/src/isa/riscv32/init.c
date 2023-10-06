889dd6ae395a18a5c3da5bffc14d66c8fb2aaf0e
tracer-ics2023 <tracer@njuics.org>
2023-10-06 17:53:44 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:53:44 up  1:24,  1 user,  load average: 0.62, 0.49, 0.44

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -24,7 +24,6 @@ static const uint32_t img [] = {
   0x0102c503,  // lbu a0,16(t0)
   0x00100073,  // ebreak (used as nemu_trap)
   0xdeadbeef,  // some data
-  0x11111111
 };
 
 static void restart() {
