a81a898c14bb71e374b9700c3ad3a955493b5515
tracer-ics2023 <tracer@njuics.org>
2023-10-08 19:09:56 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:09:56 up 27 min,  1 user,  load average: 0.39, 0.43, 0.43

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -131,7 +131,7 @@ static int cmd_p(char *args) {
       assert(0);
     }
     else {
-      printf("Done. the result of expr in hex is: 0x%08x\ndec is %u", result, result);
+      printf("Done. the result of expr in hex is: 0x%08x\nanswer in dec is %u\n", result, result);
       return 0;
     }
   }
