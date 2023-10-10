56186b64cf1742a1bb6ba812eb1ce8fdea76b08c
tracer-ics2023 <tracer@njuics.org>
2023-10-10 10:05:06 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:05:06 up 20 min,  1 user,  load average: 0.65, 0.60, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,7 +46,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   if (check_wp() == true) {
     puts(_this->logbuf);
-    printf("PC at :0x%08x\n",cpu.pc);
+    // printf("PC at :0x%08x\n",cpu.pc);
     nemu_state.state = NEMU_STOP;
   }
 }
