367b1bced7c91ac8184f8cbe2a334d9254420dbd
tracer-ics2023 <tracer@njuics.org>
2023-10-20 13:23:01 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:01 up 25 min,  1 user,  load average: 0.89, 0.70, 0.56

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,8 +34,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-// J_k J型指令的四个立即数部分
-#define immJ() do { *imm = SEXT((BITS(i, 31, 31) << 19) | BITS(i, 30, 21) | (BITS(i, 20, 20) << 10) | BITS(i, 19, 12) << 11, 20);} while(0)
+#define immJ() do { *imm = SEXT((BITS(i, 31, 31) << 19) | BITS(i, 30, 21) | BITS(i, 20, 20) << 10 | BITS(i, 19, 12) << 11, 20);} while(0)
 
 // 提取指令的各个参数
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -48,6 +47,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
+    default: break;
   }
 }
 
