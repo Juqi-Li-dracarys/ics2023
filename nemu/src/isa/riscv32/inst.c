f7624f787bd9ee42da7a003050cf0da3fcd688ab
tracer-ics2023 <tracer@njuics.org>
2023-10-20 14:26:33 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:33 up  1:28,  1 user,  load average: 0.80, 0.51, 0.45

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -34,7 +34,8 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = SEXT((BITS(i, 31, 31) << 19) | BITS(i, 30, 21) | BITS(i, 20, 20) << 10 | BITS(i, 19, 12) << 11, 20);} while(0)
+#define immJ() do { *imm = SEXT(BITS(i, 31, 31) << 19 | BITS(i, 30, 21) | BITS(i, 20, 20) << 10 | BITS(i, 19, 12) << 11, 20); } while(0)
+#define immB() do { *imm = SEXT(BITS(i, 31, 31) << 11 | BITS(i, 30, 25) << 4 | BITS(i, 11, 8) | BITS(i, 7, 7) << 10, 12); } while(0)
 
 // 提取指令的各个参数
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -47,6 +48,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_RE: src1R(); src2R();        break;
     default: break;
   }
@@ -75,6 +77,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 000 ????? 0100011", sb     , S, Mw(src1 + imm, 1, src2));
   INSTPAT("??????? ????? ????? 010 ????? 0100011", sw     , S, Mw(src1 + imm, 4, src2));
   INSTPAT("??????? ????? ????? ??? ????? 1101111", jal    , J, R(rd) = s->pc + 4; s->dnpc = s->pc + (imm << 1));
+  INSTPAT("??????? ????? ????? 000 ????? 1100011", beq    , B, s->dnpc = (src1 == src2) ? s->pc + (imm << 1) : s->dnpc);
   INSTPAT("0000000 00001 00000 000 00000 1110011", ebreak , N, NEMUTRAP(s->pc, R(10))); // 以抛出异常的方式提醒调试器 R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ???????", inv    , N, INV(s->pc));
   INSTPAT_END();
