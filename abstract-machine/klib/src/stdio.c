c4460b09c27cbd48cce8fe485ad90468d885e0e5
tracer-ics2023 <tracer@njuics.org>
2023-10-21 10:03:22 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:22 up  1:29,  1 user,  load average: 0.82, 0.62, 0.58

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,35 +14,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list args;
-  va_start(args, fmt);
-  int written = 0;  // 用于记录写入字符的数量
-  while (*fmt != '\0') {
-    if (*fmt == '%') {
-      fmt++;  // 跳过 '%'
-      if (*fmt == 's') {
-        const char *str = va_arg(args, const char *);
-        while (*str != '\0') {
-            *out++ = *str++;
-            written++;
-          }
-      } 
-      else if (*fmt == 'd') {
-        int num = va_arg(args, int);
-        int num_chars = sprintf(out, "%d", num);  // 递归调用 sprintf 处理整数
-        out += num_chars;
-        written += num_chars;
-      }
-    } 
-    else {
-      *out++ = *fmt;
-      written++;
-    }
-    fmt++;
-  }
-  *out = '\0';  // 添加 NULL 终止符
-  va_end(args);
-  return written;
+  panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
