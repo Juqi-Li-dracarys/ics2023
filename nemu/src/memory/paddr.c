59c3121e7f4aa6cb810e6fa2d827d3de987efd68
tracer-ics2023 <tracer@njuics.org>
2023-10-26 20:53:57 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:53:57 up  5:52,  1 user,  load average: 0.72, 0.87, 0.88

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -50,9 +50,9 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static void out_of_bound(paddr_t addr) {
+  IFDEF(CONFIG_ITRACE, print_ring_buffer(ring_head));
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
-  IFDEF(CONFIG_ITRACE, print_ring_buffer(ring_head));
 }
 
 void init_mem() {
