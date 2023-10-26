c8881550325f1854155a13ebeb18a67f35cf4b6f
tracer-ics2023 <tracer@njuics.org>
2023-10-26 20:56:41 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:56:41 up  5:55,  1 user,  load average: 0.62, 0.71, 0.81

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,7 +27,7 @@ const char *regs[] = {
 void isa_reg_display() {
   for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
     printf("%s:0X%08x ",regs[i], gpr(i));
-    if((i + 1) % 8 == 0)
+    if((i + 1) % 4 == 0)
     putchar('\n');
   }
   return ;
