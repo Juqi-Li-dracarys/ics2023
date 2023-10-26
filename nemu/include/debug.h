2135b6333f5ec2510b7a1396859e8a48f2b74c3a
tracer-ics2023 <tracer@njuics.org>
2023-10-26 14:29:27 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:29:27 up 49 min,  1 user,  load average: 1.50, 0.95, 0.56

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -21,7 +21,7 @@
 #include <utils.h>
 
 #define Log(format, ...) \
-    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_YELLOW) "\n", \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_CYAN) "\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 #define Assert(cond, format, ...) \
