fecb74ce6118b122fa17cb430a792b791059afde
tracer-ics2023 <tracer@njuics.org>
2023-10-31 15:26:28 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:26:28 up  2:38,  1 user,  load average: 0.83, 0.77, 0.68

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -23,7 +23,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
     if(ref_r->gpr[i] != gpr(i)) {
       printf("difftest fail @PC = 0x%08x, congratulation!\n", pc);
-      puts("register map in NEMU:");
+      puts("register map in REF:");
       for(int j = 0; j < 32; j++) {
         printf("%s:0X%08x ",regs[j], ref_r->gpr[j]);
         if((j + 1) % 4 == 0)
@@ -34,7 +34,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
   if(ref_r->pc != cpu.pc) {
     printf("difftest fail @PC = 0x%08x, congratulation!\n", pc);
-    puts("register map in NEMU:");
+    puts("register map in REF:");
     for(int j = 0; j < 32; j++) {
       printf("%s:0X%08x ",regs[j], ref_r->gpr[j]);
       if((j + 1) % 4 == 0)
