472a3960a02c6da3866a08cdb349eef8cda9de6a
tracer-ics2023 <tracer@njuics.org>
2023-11-02 21:46:10 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:10 up  7:57,  1 user,  load average: 0.59, 0.56, 0.42

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,9 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = (uint64_t)inl(RTC_ADDR) | ((uint64_t)inl(RTC_ADDR + 4) << 32);
+  uint64_t us_lower = (uint64_t)inl(RTC_ADDR);
+  uint64_t us_higher = (uint64_t)inl(RTC_ADDR + 4);
+  uptime->us = us_lower | (us_higher << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -21,7 +21,8 @@ static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 4) {
+  //注意，这里 offset = 4 读高32位时不会更新（已经修正bug）
+  if (!is_write && offset == 0) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
