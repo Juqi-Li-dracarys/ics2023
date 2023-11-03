dee5dcb5560991560df69a0f9f76970bdff30376
tracer-ics2023 <tracer@njuics.org>
2023-11-03 12:12:38 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:12:38 up  3:01,  1 user,  load average: 0.79, 0.81, 0.92

--- abstract-machine/am/src/native/ioe.c
+++ abstract-machine/am/src/native/ioe.c
@@ -79,3 +79,11 @@ static void do_io(int reg, void *buf) {
 
 void ioe_read (int reg, void *buf) { do_io(reg, buf); }
 void ioe_write(int reg, void *buf) { do_io(reg, buf); }
+
+
+/*
+ 整个ioe的流程大概如下：
+ 1. 客户程序调用封装好的 io_read(reg) 或者 io_write(reg, ...)，里面包含临时变量，注意串口比较特殊，API直接是putch(), 无ioe
+ 2. 调用ioe_write 或者 read, 进一步调用do_io,利用 reg 在 lut 中找到对应的ioe函数并执行
+ 3. 执行函数会将值存入临时变量，结束后io_read可以将临时变量return
+*/
