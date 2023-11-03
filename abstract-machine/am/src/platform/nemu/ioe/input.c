049e11876f90c64e44392528897eccf3691541e1
tracer-ics2023 <tracer@njuics.org>
2023-11-03 13:54:27 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:27 up  4:43,  1 user,  load average: 0.99, 1.08, 0.84

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t read_data = inl(KBD_ADDR);
-  kbd->keydown = (read_data & KEYDOWN_MASK ? true : false);
-  kbd->keycode = read_data & ~KEYDOWN_MASK;
+  uint32_t data = inl(KBD_ADDR);
+  kbd->keydown = (data & KEYDOWN_MASK ? true : false);
+  kbd->keycode = data & ~KEYDOWN_MASK;
 }
