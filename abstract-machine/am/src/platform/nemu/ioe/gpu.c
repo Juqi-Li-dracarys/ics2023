6976e734597923f0c0cb144719ff5f843f155ada
tracer-ics2023 <tracer@njuics.org>
2023-11-04 01:28:14 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:28:14 up  4:20,  1 user,  load average: 0.97, 1.17, 1.01

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -11,7 +11,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR),
-    .vmemsz = 0
+    .vmemsz = inw(VGACTL_ADDR + 2) * inw(VGACTL_ADDR) * sizeof(uint32_t)
   };
 }
 
@@ -25,7 +25,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->w == 0 || ctl->h == 0) return;
   for(j = ctl->y; j < ctl->y + ctl->h; j++) {
     for(i = ctl->x; i < ctl->x + ctl->w; i++) {
-      if(i >= 0 && i < x_max && j >= 0 && j < y_max && ctl->pixels == NULL) {
+      if(i >= 0 && i < x_max && j >= 0 && j < y_max && ctl->pixels != NULL) {
         outl(FB_ADDR + (i + j * x_max) * sizeof(uint32_t), *((uint32_t *)(ctl->pixels) + k));
       }
       k++;
