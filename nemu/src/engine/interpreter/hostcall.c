1b166e793118d9f67ec1b88c3d9baa22680e2492
tracer-ics2023 <tracer@njuics.org>
2023-11-19 10:42:06 +0800

    >  compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:42:06 up  1:35,  1 user,  load average: 0.72, 0.35, 0.28

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -14,7 +14,7 @@
 ***************************************************************************************/
 
 #include <utils.h>
-// #include <cpu/ifetch.h>
+#include <cpu/ifetch.h>
 #include <isa.h>
 #include <cpu/difftest.h>
 
@@ -28,9 +28,9 @@ void set_nemu_state(int state, vaddr_t pc, int halt_ret) {
 __attribute__((noinline))
 void invalid_inst(vaddr_t thispc) {
   uint32_t temp[2];
-  // vaddr_t pc = thispc;
-  // temp[0] = inst_fetch(&pc, 4);
-  // temp[1] = inst_fetch(&pc, 4);
+  vaddr_t pc = thispc;
+  temp[0] = inst_fetch(&pc, 4);
+  temp[1] = inst_fetch(&pc, 4);
 
   uint8_t *p = (uint8_t *)temp;
   printf("invalid opcode(PC = " FMT_WORD "):\n"
