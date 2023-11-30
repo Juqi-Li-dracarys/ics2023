3838072200621e7140be2c6df2d83341904e98b9
tracer-ics2023 <tracer@njuics.org>
2023-12-01 00:46:12 +0800

    > imp RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:46:12 up  1:33,  1 user,  load average: 0.76, 0.58, 0.41

--- npc/project/lab6/vsrc/top.v
+++ npc/project/lab6/vsrc/top.v
@@ -2,6 +2,7 @@
 module top (
     input clk,
     input rst,
+    input my_clk,
     output [7:0] seg_1,
     output [7:0] seg_2
 );
@@ -9,7 +10,7 @@ module top (
     wire [3:0] seg_2_t;
 
     shifter shifter_8 (
-        .clk(clk),
+        .clk(my_clk),
         .rst(rst),
         .dout({seg_2_t, seg_1_t})
     );
