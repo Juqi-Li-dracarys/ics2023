fcd5837ccbdfcf503f6ee410f9db83f20ffae597
tracer-ics2023 <tracer@njuics.org>
2023-12-01 22:18:02 +0800

    > imp RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:18:02 up  2:18,  1 user,  load average: 1.13, 0.92, 0.75

--- npc/project/lab7/vsrc/top.v
+++ npc/project/lab7/vsrc/top.v
@@ -2,29 +2,50 @@
 module top (
     input clk,
     input rst,
-    input my_clk,
+    input ps2_clk,
+    input ps2_data,
     output [7:0] seg_1,
     output [7:0] seg_2
 );
+
     wire [3:0] seg_1_t;
     wire [3:0] seg_2_t;
+    wire ready;
+    wire overflow;
+    reg read_n;
 
-    shifter shifter_8 (
-        .clk(my_clk),
-        .rst(rst),
-        .dout({seg_2_t, seg_1_t})
+    ps2_keyboard key_board(
+        .clk(clk),
+        .rst_n(~rst),
+        .ps2_clk(ps2_clk),
+        .ps2_data(ps2_data),
+        .read_n(read_n),
+        .data({seg_2_t, seg_1_t}),
+        .ready(ready),
+        .overflow(overflow)
     );
-    seg_decode_hex hex_1 (
-        .en(~rst),
+
+    seg_decode_hex seg1(
         .in(seg_1_t),
-        .out(seg_1) 
-    );
-    seg_decode_hex hex_2 (
         .en(~rst),
-        .in(seg_2_t),
-        .out(seg_2) 
+        .out(seg_1)
     );
+
+    seg_decode_hex seg2(
+        .in(seg_2_t),
+        .en(~rst),
+        .out(seg_2)
+    );
+
+    always @(negedge clk, posedge rst) begin
+        if(rst == 1'b1) begin
+            read_n <= 1'b1;
+        end
+        else begin
+            read_n <= ~ready;
+        end
+    end
+
     
 endmodule
-
 
