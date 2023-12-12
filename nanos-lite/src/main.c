99c50b2a7f08cc71e37cbca99fa96d83e39ba8d8
tracer-ysyx <tracer@ysyx.org>
2023-12-12 21:16:32 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:32 up  7:42,  1 user,  load average: 1.76, 1.02, 0.81

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -9,8 +9,10 @@ void init_proc(void);
 
 int main() {
   extern const char logo[];
+  
+  printf("\33[1;35m[]" "hello" "\n");
   printf("%s", logo);
-  printf("\n" "hello" "\n");
+  printf("\33[1;35m[]" "hello" "\n");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
