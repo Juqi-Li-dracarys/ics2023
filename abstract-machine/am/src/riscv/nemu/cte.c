a2209685c07b014cc1297c8dd39364584be3cab1
tracer-ysyx <tracer@ysyx.org>
2023-12-12 15:11:42 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:42 up  2:25,  1 user,  load average: 0.51, 0.43, 0.41

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
 
     for(int i = 0; i < 32; i++) {
-       printf("0X%08x ", c->gpr[i]);
+       printf("%d", c->gpr[i]);
     }
 
     Event ev = {0};
