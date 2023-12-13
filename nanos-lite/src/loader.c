172dd3f86e95e7259ce49c2d026c00388d61d829
tracer-ics2023 <tracer@njuics.org>
2023-12-13 21:59:02 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:02 up  6:50,  1 user,  load average: 0.23, 0.42, 0.38

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,10 +13,17 @@ extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 
 // 解读 elf 文件内容，将程序指令拷贝到正确位置的
-// 好奇的鼠鼠会想：这和 NEMU 加载 AM 的程序有啥区别？
-// 区别在于 AM 对 ELF 文件进行 objdump，再转化为 bin
-// 之后 copy 到 memory, 接下来取指执行
-// 而这里我们要直接对 ELF 解析
+// 好奇的鼠鼠会想：这和 NEMU 加载 AM 的程序的过程有啥区别？
+// 区别在于 AM 对 elf 文件进行 objcopy 转化为 bin
+// 之后 copy 到 memory, 接下来取指执行, 而这里我们要直接对 ELF 解析
+
+// 想我这样的蠢鼠鼠还会思考，程序从可执行文件加载包含数据加载和代码加载等
+// 为啥我看加载到 NEMU 的过程只是把 elf 文件粗暴地拷贝过去，而没有解析代码和数据呢？
+// 我们能不能在这里用对 NEMU 的骚操作来完成 loader ？
+// 答案是不能
+// 那是因为 elf 用的 ld 链接器，其链接后的格式被精心处理过了，
+// 也因为它加载到了 0x80000000, 内存的开端，细品....
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   TODO();
   return 0;
