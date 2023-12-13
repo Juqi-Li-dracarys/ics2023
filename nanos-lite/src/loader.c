59cd637c54531f468c0c2cbcb91614ef9bc29125
tracer-ics2023 <tracer@njuics.org>
2023-12-13 21:08:25 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:08:25 up  5:59,  1 user,  load average: 0.50, 0.39, 0.29

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,6 +9,14 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+
+// 解读 elf 文件内容，将程序指令拷贝到正确位置的
+// 好奇的鼠鼠会想：这和 NEMU 加载 AM 的程序有啥区别？
+// 区别在于 AM 对 ELF 文件进行 objdump，再转化为 bin
+// 之后 copy 到 memory, 接下来取指执行
+// 而这里我们要直接对 ELF 解析
 static uintptr_t loader(PCB *pcb, const char *filename) {
   TODO();
   return 0;
@@ -17,6 +25,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %p", entry);
+  // 过调用这个函数指针，实际上执行了加载文件的代码
   ((void(*)())entry) ();
 }
 
