42e8d63326bbd2368f197161df37e0edf838632d
tracer-ysyx <tracer@ysyx.org>
2023-12-14 20:00:43 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:00:43 up 57 min,  1 user,  load average: 0.17, 0.35, 0.34

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,18 +1,14 @@
-#include <common.h>
 #include "syscall.h"
-
-uint32_t SYS_yield() {
-  yield();
-  return 0;
-}
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  // 系统调用号
   switch (a[0]) {
-    case 0x01: SYS_yield(); break;
+    case SYS_yield: a[3] = sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+
 }
 
 
