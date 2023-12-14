60242423441a3180da87df75f6edb063c3b3d4dc
tracer-ysyx <tracer@ysyx.org>
2023-12-14 20:44:57 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:44:57 up  1:41,  1 user,  load average: 0.41, 0.29, 0.32

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -13,13 +13,13 @@ struct Context {
   void *pdir;
 };
 
+// macro for syscall
+// ARGS_ARRAY ("ecall", "a7", "a0", "a1", "a2", "a0")
 #ifdef __riscv_e
 #define GPR1 gpr[15] // a5
 #else
 #define GPR1 gpr[17] // a7
 #endif
-
-// ARGS_ARRAY ("ecall", "a7", "a0", "a1", "a2", "a0")
 
 #define GPR2 gpr[10] // a0
 #define GPR3 gpr[11] // a1
