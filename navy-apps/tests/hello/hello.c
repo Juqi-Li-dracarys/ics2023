443887382f0b6b08e8d013fc7bc4919e1da6e158
tracer-ysyx <tracer@ysyx.org>
2023-12-15 17:47:34 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:34 up  4:54,  1 user,  load average: 0.75, 0.53, 0.48

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,15 +4,15 @@
 int main() {
   printf("Hello\n");
   write(1, "Hello World!\n", 13);
-  // int i = 0;
-  // volatile int j = 0;
+  int i = 0;
+  volatile int j = 0;
 
-  // while (i < 5) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
+  while (i < 5) {
+    j ++;
+    if (j == 10000) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      j = 0;
+    }
+  }
   return 0;
 }
