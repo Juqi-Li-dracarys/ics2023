63f9ea453a1ec9f5ab06c2d729a19eff6c4978b0
tracer-ics2023 <tracer@njuics.org>
2023-12-15 15:43:35 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:35 up  2:50,  1 user,  load average: 1.23, 1.13, 0.82

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,14 @@
 #include "syscall.h"
 
+// 处理各种系统调用号
 void do_syscall(Context *c) {
-  // 系统调用号
   switch (c->GPR1) {
     case SYS_yield: c->GPRx = sys_yield(); break;
     case SYS_exit: sys_exit(c->GPR2); break;
     case SYS_write: c->GPRx = sys_write(c->GPR2, (char *)(c->GPR3), c->GPR4); break;
+    case SYS_brk: c->GPRx = sys_brk((uintptr_t *)(c->GPR2), c->GPR3); break;
     default: panic("Unhandled syscall ID = %d", c->GPR1);
   }
 }
-
 
 
