eeb7a4558b4f15f8977c8827ea4d6851d9c9da9c
tracer-ics2023 <tracer@njuics.org>
2023-12-15 16:43:28 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:43:28 up  3:49,  1 user,  load average: 1.29, 1.02, 0.90

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,50 @@
 #include "syscall.h"
 
+#define MAX_NUM 100
+
+static s_node *head = NULL;
+static uintptr_t size = 0;
+
+void add_strace(uintptr_t type, uintptr_t arg0, uintptr_t arg1, uintptr_t arg2, uintptr_t ret) {
+#ifdef STRACE
+  if(size < MAX_NUM) {
+    s_node *new = (s_node *)malloc(sizeof(s_node));
+    new->type = type;
+    new->arg_ret[0] = arg0;
+    new->arg_ret[1] = arg1;
+    new->arg_ret[2] = arg2;
+    new->arg_ret[3] = ret;
+    new->next = head;
+    head = new;
+    size++;
+  }
+#endif
+  return;
+}
+
+void disp_strace(void) {
+#ifdef STRACE
+  s_node *temp = head;
+  uintptr_t i = 0;
+  while(temp != NULL) {
+    printf("[%d]: type: %d, arg0:%p, arg1:%p, arg2:%p, ret:%p\n", i, temp->type, temp->arg_ret[0], temp->arg_ret[1], temp->arg_ret[2], temp->arg_ret[3]);
+    i++;
+    temp = temp->next;
+  }
+#endif
+  return;
+}
+
 // 处理各种系统调用号
 void do_syscall(Context *c) {
   switch (c->GPR1) {
-    case SYS_yield: c->GPRx = sys_yield(); break;
-    case SYS_exit: sys_exit(c->GPR2); break;
-    case SYS_write: c->GPRx = sys_write(c->GPR2, (char *)(c->GPR3), c->GPR4); break;
-    case SYS_brk: c->GPRx = sys_brk((uintptr_t *)(c->GPR2), c->GPR3); break;
+    case SYS_yield: c->GPRx = sys_yield(); add_strace(SYS_yield, 0, 0, 0, c->GPRx); break;
+    case SYS_exit: disp_strace(); sys_exit(c->GPR2); break;
+    case SYS_write: c->GPRx = sys_write(c->GPR2, (char *)(c->GPR3), c->GPR4); add_strace(SYS_write, c->GPR2, c->GPR3, c->GPR4, c->GPRx); break;
+    case SYS_brk: c->GPRx = sys_brk((uintptr_t *)(c->GPR2), c->GPR3); add_strace(SYS_brk, c->GPR2, c->GPR3, 0, c->GPRx); break;
     default: panic("Unhandled syscall ID = %d", c->GPR1);
   }
 }
+
 
 
