6b388993f10ed70b3680d0253248e1dc49fbfc18
tracer-ysyx <tracer@ysyx.org>
2023-12-15 15:49:34 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:34 up  2:56,  1 user,  load average: 1.43, 1.28, 1.00

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -78,7 +78,7 @@ int _write(int fd, void *buf, size_t count) {
 void *_sbrk(intptr_t increment) {
   uintptr_t old_break = prog_break;
   if(!_syscall_(SYS_brk, (uintptr_t)(&prog_break), increment, 0))
-    return 0;
+    return old_break;
   else
     return -1;
 }
