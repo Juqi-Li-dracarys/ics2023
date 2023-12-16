231bc8fc0c8a3d42ed5d25122f2ccd301b744377
tracer-ysyx <tracer@ysyx.org>
2023-12-16 10:11:39 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:39 up  1:15,  1 user,  load average: 0.98, 0.80, 0.65

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -76,7 +76,13 @@ size_t lseek(int fd, size_t offset, int whence) {
       return -1;
     }
   }
-  return file_table[fd].open_offset;
+  if(file_table[fd].open_offset + file_table[fd].disk_offset < file_table[fd].disk_offset + file_table[fd].size && file_table[fd].open_offset + file_table[fd].disk_offset >= file_table[fd].disk_offset)  {
+    return file_table[fd].open_offset;
+  }
+  else {
+    panic("error: offset overflow");
+    return -1;
+  }
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
@@ -85,6 +91,14 @@ size_t fs_read(int fd, void *buf, size_t len) {
     return -1;
   }
   return ramdisk_read(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if(fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR || fd >= sizeof(file_table) / sizeof(Finfo)) {
+    panic("error: can't write error file");
+    return -1;
+  }
+  return ramdisk_write(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
 }
 
 int fs_close(int fd) {
