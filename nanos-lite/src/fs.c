0521e327da0140483c74f9e04684057e72256ab4
tracer-ysyx <tracer@ysyx.org>
2023-12-16 09:53:12 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:53:12 up 56 min,  1 user,  load average: 0.48, 0.49, 0.51

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -37,7 +37,10 @@ void init_fs() {
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
-  if(pathname == NULL) return -1;
+  if(pathname == NULL) {
+    Log("warning: can't open file pathname = %s.", pathname); 
+    return -1;
+  }
   for(uintptr_t i = 0; i < sizeof(file_table) / sizeof(Finfo); i++) {
     if(strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
