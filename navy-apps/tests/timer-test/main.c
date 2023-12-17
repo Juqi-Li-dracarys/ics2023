9c9cf487932a226b2b5df1f697f55fd56d8106ec
tracer-ics2023 <tracer@njuics.org>
2023-12-17 14:06:58 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:58 up  1:26,  1 user,  load average: 0.44, 0.60, 0.66

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,22 +4,21 @@
 #include <stdint.h>
 
 int main() {
-  NDL_Init(0);
-  uint32_t last_time = NDL_GetTicks();
-  uint32_t now_time = NDL_GetTicks();
+  // NDL_Init(0);
+  // uint32_t last_time = NDL_GetTicks();
+  // uint32_t now_time = NDL_GetTicks();
 
   struct timeval tv = {0};
   gettimeofday(&tv, NULL);
-  printf("hello word @ %d ms.\n", now_time);
   printf("hello word @ %d us.\n", (uint32_t)tv.tv_usec);
 
-  while(now_time < 10000) {
-    now_time = NDL_GetTicks();
-    if(now_time - last_time >= 500) {
-      printf("hello word @ %d ms.\n", now_time);
-      last_time = now_time;
-    }
-  }
-  NDL_Quit();
+  // while(now_time < 10000) {
+  //   now_time = NDL_GetTicks();
+  //   if(now_time - last_time >= 500) {
+  //     printf("hello word @ %d ms.\n", now_time);
+  //     last_time = now_time;
+  //   }
+  // }
+  // NDL_Quit();
   return 0;
 }
