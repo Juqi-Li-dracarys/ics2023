59767fd96fac9a38a40dda2bc1a03a02c55f1c4d
tracer-ics2023 <tracer@njuics.org>
2023-12-17 14:17:36 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:36 up  1:36,  1 user,  load average: 0.28, 0.36, 0.51

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,7 +10,7 @@ int main() {
 
   struct timeval tv = {0};
   gettimeofday(&tv, NULL);
-  printf("hello word @ %d us.\n", (uint32_t)tv.tv_usec);
+  printf("hello word @ %d us.\n", (uint32_t)(tv.tv_usec << 32));
 
   // while(now_time < 10000) {
   //   now_time = NDL_GetTicks();
