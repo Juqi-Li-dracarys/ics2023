28d517defa7371e75a9f1bd5c67999a478f6f6f9
tracer-ysyx <tracer@ysyx.org>
2023-12-17 13:56:15 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:56:15 up  1:15,  1 user,  load average: 1.22, 0.88, 0.72

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -7,6 +7,7 @@ int main() {
   NDL_Init(0);
   uint32_t last_time = NDL_GetTicks();
   uint32_t now_time = NDL_GetTicks();
+  printf("hello word @ %d ms.\n", now_time);
   while(now_time < 10000) {
     now_time = NDL_GetTicks();
     if(now_time - last_time >= 500) {
