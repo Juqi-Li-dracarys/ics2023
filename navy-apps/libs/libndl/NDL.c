9ceb771ffcb0a68bb3d023248e03f8611029a0b5
tracer-ics2023 <tracer@njuics.org>
2023-12-17 22:49:09 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:49:09 up  3:49,  1 user,  load average: 0.65, 0.77, 0.63

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -12,7 +12,9 @@ static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
+// FILE pointer
 static int event_fp = -1;
+static int info_fp = -1;
 
 // return system time in ms
 uint32_t NDL_GetTicks() {
@@ -30,6 +32,18 @@ int NDL_PollEvent(char *buf, int len) {
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
+  int max_width = 0;
+  int max_height = 0;
+  if(info_fp == -1) {
+    info_fp = open("/proc/dispinfo", 0, 0);
+  }
+  char buf[20] = {0};
+  if(read(info_fp, buf, 20)) {
+    sscanf(buf, "WIDTH:%d HEIGHT:%d", &max_width, &max_height);
+  }
+  printf("%d  %d\n", max_width, max_height);
+
+  // ignore it
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
@@ -47,6 +61,7 @@ void NDL_OpenCanvas(int *w, int *h) {
     }
     close(fbctl);
   }
+  
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
