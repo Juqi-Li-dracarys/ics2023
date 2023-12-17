551d8ab028d805febd1c0087937ec5896a857d31
tracer-ysyx <tracer@ysyx.org>
2023-12-17 13:26:05 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:05 up 45 min,  1 user,  load average: 0.89, 0.88, 0.78

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -6,12 +6,12 @@
 int main() {
   struct timeval tv = {0};
   gettimeofday(&tv, NULL);
-  uint64_t last_time = tv.tv_usec;
+  uint64_t last_time = tv.tv_sec;
   while(1) {
     gettimeofday(&tv, NULL);
-    if(tv.tv_sec - last_time >= 500000) {
+    if(tv.tv_sec - last_time >= 1) {
       printf("hello word.\n");
-      last_time = tv.tv_usec;
+      last_time = tv.tv_sec;
     }
   }
   return 0;
