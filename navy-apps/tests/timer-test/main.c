7dd57df29e699b796c0b61e86de5268e1b125697
tracer-ysyx <tracer@ysyx.org>
2023-12-17 13:48:37 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:48:37 up  1:07,  1 user,  load average: 0.98, 0.65, 0.60

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,18 +1,19 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 #include <stdint.h>
 
 int main() {
-  struct timeval tv = {0};
-  gettimeofday(&tv, NULL);
-  uint64_t last_time = tv.tv_sec;
-  while(1) {
-    gettimeofday(&tv, NULL);
-    if(tv.tv_sec - last_time >= 1) {
-      printf("hello word.\n");
-      last_time = tv.tv_sec;
+  NDL_Init(0);
+  uint32_t last_time = NDL_GetTicks();
+  uint32_t now_time = NDL_GetTicks();
+  while(now_time < 10000) {
+    now_time = NDL_GetTicks();
+    if(now_time - last_time >= 500) {
+      printf("hello word @ %d ms.\n", now_time);
+      last_time = now_time;
     }
   }
+  NDL_Quit();
   return 0;
 }
