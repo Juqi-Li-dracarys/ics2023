577bd53233356b40743b5afc20ca8a06d11d8315
tracer-ysyx <tracer@ysyx.org>
2023-12-18 14:41:56 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:56 up  2:36,  1 user,  load average: 0.57, 0.85, 0.95

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -70,7 +70,6 @@ size_t fb_write(const void* buf, size_t offset, size_t len) {
   }
   uint32_t i = pix_offset % max_width;
   uint32_t j = pix_offset / max_width;
-  printf("width: %d height: %d\n", max_width, max_height);
   printf("offset:%d x:%d y:%d\n",pix_offset, i, j);
 
   while (pix_in < pix_len && (i < max_width || j < max_height)) {
