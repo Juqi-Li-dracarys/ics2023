f7a90a403a4385583828f5db08391d79cc1e43f9
tracer-ysyx <tracer@ysyx.org>
2023-12-18 23:18:52 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:52 up  8:05,  1 user,  load average: 2.08, 1.33, 1.05

--- navy-apps/tests/fixed-test/main.c
+++ navy-apps/tests/fixed-test/main.c
@@ -24,10 +24,9 @@ int main() {
     assert(fixedpt_toint(fixedpt_ceil(D)) == -1);
 
     D = fixedpt_div(A, B);
-    printf("A: %x\n", fixedpt_toint(A));
-    printf("B: %x\n", fixedpt_toint(B));
-    printf("D: %x\n", fixedpt_toint(D));
-    printf("%d\n", D);
+    printf("A: %x\n", (A));
+    printf("B: %x\n", (B));
+    printf("D: %x\n", (D));
     assert(fixedpt_toint(D) == -2);
     assert(fixedpt_toint(fixedpt_floor(D)) == -2);
     assert(fixedpt_toint(fixedpt_ceil(D)) == -1);
