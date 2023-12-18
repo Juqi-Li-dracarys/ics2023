477bc017638580f74c9f69d0e6ab19d70cccb24f
tracer-ics2023 <tracer@njuics.org>
2023-12-18 21:56:30 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:30 up  7:25,  1 user,  load average: 2.01, 1.30, 1.08

--- navy-apps/tests/fixed-test/main.c
+++ navy-apps/tests/fixed-test/main.c
@@ -4,14 +4,14 @@
 #include <assert.h>
 
 int main() {
-    fixedpt A = fixedpt_rconst(-3.1);
+    fixedpt A = fixedpt_rconst(-3.3);
     fixedpt B = fixedpt_rconst(3);
-    int C = -3;
+    int C = 3;
     fixedpt D = 0;
 
     D = fixedpt_muli(A, C);
-    printf("%d\n", fixedpt_toint(A));
-    assert(fixedpt_toint(D) == -9);
+    assert(fixedpt_toint(A) == -4);
+    assert(fixedpt_toint(D) == -10);
     assert(fixedpt_toint(fixedpt_floor(D)) == -10);
     assert(fixedpt_toint(fixedpt_ceil(D)) == -9);
 
