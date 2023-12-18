1ae02d448c99ea3c0d8b4b320018c404a2ad254a
tracer-ysyx <tracer@ysyx.org>
2023-12-19 00:16:21 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:16:21 up  9:03,  1 user,  load average: 0.99, 0.95, 0.85

--- navy-apps/tests/fixed-test/main.c
+++ navy-apps/tests/fixed-test/main.c
@@ -4,32 +4,36 @@
 #include <assert.h>
 
 int main() {
-    fixedpt A = fixedpt_rconst(-3.3);
-    fixedpt B = fixedpt_rconst(3);
-    int C = 3;
+    fixedpt A = fixedpt_rconst(-411.98);
+    fixedpt B = fixedpt_rconst(30.07);
+    int C = -7;
     fixedpt D = 0;
+    assert(fixedpt_toint(A) == -412);
+    assert(fixedpt_toint(B) == 30);
 
     D = fixedpt_muli(A, C);
-    assert(fixedpt_toint(A) == -4);
-    assert(fixedpt_toint(D) == -10);
-    assert(fixedpt_toint(fixedpt_floor(D)) == -10);
-    assert(fixedpt_toint(fixedpt_ceil(D)) == -9);
-    assert(fixedpt_toint(fixedpt_abs(fixedpt_floor(D))) == 10);
+    assert(fixedpt_toint(D) == 2883);
+    assert(fixedpt_toint(fixedpt_floor(D)) == 2883);
+    assert(fixedpt_toint(fixedpt_ceil(D)) == 2884);
+    assert(fixedpt_toint(fixedpt_abs(fixedpt_floor(D))) == 2883);
+
     D = fixedpt_mul(A, B);
-    assert(fixedpt_toint(D) == -10);
-    assert(fixedpt_toint(fixedpt_floor(D)) == -10);
-    assert(fixedpt_toint(fixedpt_ceil(D)) == -9);
-    assert(fixedpt_toint(fixedpt_abs(fixedpt_ceil(D))) == 9);
-    D = fixedpt_divi(A, fixedpt_toint(B));
-    assert(fixedpt_toint(D) == -2);
-    assert(fixedpt_toint(fixedpt_floor(D)) == -2);
-    assert(fixedpt_toint(fixedpt_ceil(D)) == -1);
-    assert(fixedpt_toint(fixedpt_abs(fixedpt_floor(D))) == 2);
+    assert(fixedpt_toint(D) == -12389);
+    assert(fixedpt_toint(fixedpt_floor(D)) == -12389);
+    assert(fixedpt_toint(fixedpt_ceil(D)) == -12388);
+    assert(fixedpt_toint(fixedpt_abs(fixedpt_ceil(D))) == 12388);
+
+    D = fixedpt_divi(A, C);
+    assert(fixedpt_toint(D) == 58);
+    assert(fixedpt_toint(fixedpt_floor(D)) == 58);
+    assert(fixedpt_toint(fixedpt_ceil(D)) == 59);
+    assert(fixedpt_toint(fixedpt_abs(fixedpt_floor(D))) == 58);
+
     D = fixedpt_div(A, B);
-    assert(fixedpt_toint(D) == -2);
-    assert(fixedpt_toint(fixedpt_floor(D)) == -2);
-    assert(fixedpt_toint(fixedpt_ceil(D)) == -1);
-    assert(fixedpt_toint(fixedpt_abs(fixedpt_ceil(D))) == 1);
+    assert(fixedpt_toint(D) == -14);
+    assert(fixedpt_toint(fixedpt_floor(D)) == -14);
+    assert(fixedpt_toint(fixedpt_ceil(D)) == -13);
+    assert(fixedpt_toint(fixedpt_abs(fixedpt_ceil(D))) == 13);
 
     printf("PASS\n");
     return 0;
