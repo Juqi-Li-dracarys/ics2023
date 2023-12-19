75a048594179a01b0c5a4af8782e127d4137831e
tracer-ics2023 <tracer@njuics.org>
2023-12-19 11:04:46 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:04:46 up  2:16,  1 user,  load average: 1.53, 1.47, 1.27

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,26 +17,28 @@ int SDL_PollEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_WaitEvent(SDL_Event *event) {
+int SDL_WaitEvent(SDL_Event* event) {
   char type = 0;
-  char buf [15] = {0};
-  char name [10] = {0};
-  if(NDL_PollEvent(buf, sizeof(buf))) {
-    // parsing the event
-    sscanf(buf, "%c %s\n", &type, name);
-    printf("%c\n%s\n", type, name);
-    if(type == 'd') 
-      event->type = SDL_KEYDOWN;
-    else 
-      event->type = SDL_KEYUP;
-    for(int i = 0; i < sizeof(keyname)/sizeof(char *); i++) {
-      if(strcmp(name, keyname[i]) == 0) {
-        event->key.keysym.sym = i;
-        break;
+  char buf[15] = { 0 };
+  char name[10] = { 0 };
+  while (1) {
+    if (NDL_PollEvent(buf, sizeof(buf))) {
+      // parsing the event
+      sscanf(buf, "%c %s\n", &type, name);
+      printf("%c\n%s\n", type, name);
+      if (type == 'd')
+        event->type = SDL_KEYDOWN;
+      else
+        event->type = SDL_KEYUP;
+      for (int i = 0; i < sizeof(keyname) / sizeof(char*); i++) {
+        if (strcmp(name, keyname[i]) == 0) {
+          event->key.keysym.sym = i;
+          return 1;
+        }
       }
+      return 0;
     }
   }
-  return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
