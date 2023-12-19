00ae743317f12b4870ab2134a7bc786ca4fd4c33
tracer-ics2023 <tracer@njuics.org>
2023-12-19 23:28:36 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:28:36 up  3:29,  1 user,  load average: 1.25, 1.07, 0.96

--- navy-apps/tests/eof-test/main.c
+++ navy-apps/tests/eof-test/main.c
@@ -4,7 +4,7 @@ int main() {
     FILE *fp = fopen("/share/files/test", "r");
     int times = 0;
     char buf [10] = {0};
-    while (!feof(fp) && times < 4)
+    while (!feof(fp) && times < 5)
     {
         fscanf(fp, "%s\n", buf);
         printf("[%d]  eof:%d  %s\n", times, feof(fp), buf);
