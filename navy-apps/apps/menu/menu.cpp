f236b2d0f21d3241be1939d99fcc1f084ee977b7
tracer-ics2023 <tracer@njuics.org>
2023-12-19 17:01:54 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:01:54 up  7:29,  1 user,  load average: 1.19, 0.64, 0.44

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -184,9 +184,4 @@ static void display_menu(int n) {
   printf("========================================\n");
   printf("Please Choose.\n");
   fflush(stdout);
-  while (1)
-  {
-    /* code */
-  }
-  
 }
