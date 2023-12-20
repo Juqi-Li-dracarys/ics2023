9ee87560de25b8dd755ea45fb19f7769c0e6eedf
tracer-ysyx <tracer@ysyx.org>
2023-12-20 11:02:41 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:02:41 up  2:15,  1 user,  load average: 1.89, 1.32, 1.05

--- navy-apps/tests/eof-test/main.c
+++ navy-apps/tests/eof-test/main.c
@@ -4,18 +4,18 @@
 // 目前问题定位在 libos 和 libc
 // 即两个 syscall
 int main() {
-    // FILE *fp = fopen("/share/files/test", "r");
-    // int times = 0;
-    // char buf [10] = {0};
-    // while (!feof(fp) && times < 1)
-    // {
-    //     // fscanf(fp, "%s\n", buf);
-    //     int size_in = fread(buf, 7, 1, fp);
-    //     printf("[%d]  eof:%d  offset:%d  size:%d  str:%s", times, feof(fp), ftell(fp), size_in, buf);
-    //     times++;
-    // }
-    printf("hello world\n");
-    printf("********************\n");
+    FILE *fp = fopen("/share/files/test", "r");
+    int times = 0;
+    char buf [10] = {0};
+    while (!feof(fp) && times < 1)
+    {
+        // fscanf(fp, "%s\n", buf);
+        int size_in = fread(buf, 7, 1, fp);
+        printf("[%d]  eof:%d  offset:%d  size:%d  str:%s", times, feof(fp), ftell(fp), size_in, buf);
+        times++;
+    }
+    // printf("hello world\n");
+    // printf("********************\n");
     return 0;
     
 }
