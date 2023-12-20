a6d709f7c9dbe1efd215e9f0a065b5855546c340
tracer-ysyx <tracer@ysyx.org>
2023-12-20 13:35:28 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:35:28 up  4:00,  1 user,  load average: 1.71, 1.56, 1.29

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -97,8 +97,8 @@ size_t fs_read(int fd, void *buf, size_t len) {
   }
   // 文件越界检查, std 跳过
   if((fd < DEVICE_NUM) || (file_table[fd].open_offset <= file_table[fd].size && file_table[fd].open_offset >= 0))  {
-    len = len < file_table[fd].size - file_table[fd].open_offset ? len : file_table[fd].size - file_table[fd].open_offset;
-    size_t f_size = file_table[fd].read(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
+    size_t length = len < file_table[fd].size - file_table[fd].open_offset ? len : file_table[fd].size - file_table[fd].open_offset;
+    size_t f_size = file_table[fd].read(buf, file_table[fd].open_offset + file_table[fd].disk_offset, length);
     file_table[fd].open_offset += f_size;
     return f_size;
   }
@@ -116,8 +116,8 @@ size_t fs_write(int fd, const void *buf, size_t len) {
   }
   // 文件越界检查, std 跳过
   if((fd < DEVICE_NUM) || (file_table[fd].open_offset <= file_table[fd].size && file_table[fd].open_offset >= 0)) {
-    len = len < file_table[fd].size - file_table[fd].open_offset ? len : file_table[fd].size - file_table[fd].open_offset;
-    size_t f_size = file_table[fd].write(buf, file_table[fd].open_offset + file_table[fd].disk_offset, len);
+    size_t length = len < file_table[fd].size - file_table[fd].open_offset ? len : file_table[fd].size - file_table[fd].open_offset;
+    size_t f_size = file_table[fd].write(buf, file_table[fd].open_offset + file_table[fd].disk_offset, length);
     file_table[fd].open_offset += f_size;
     return f_size;
   }
