cd700c582ea3c1e1faf3ca6a9c409f5300786e18
tracer-ics2023 <tracer@njuics.org>
2023-12-20 13:54:55 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:54:55 up  4:05,  1 user,  load average: 1.72, 1.49, 1.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -28,12 +28,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
-  [FD_INFO]   = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, invalid_read, fb_write},
+  [FD_STDIN]  = {"stdin", 0x80000000, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0x80000000, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0x80000000, 0, invalid_read, serial_write},
+  [FD_EVENTS] = {"/dev/events", 0x80000000, 0, events_read, invalid_write},
+  [FD_INFO]   = {"/proc/dispinfo", 0x80000000, 0, dispinfo_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0x80000000, 0, invalid_read, fb_write},
 #include "files.h"
 };
 
