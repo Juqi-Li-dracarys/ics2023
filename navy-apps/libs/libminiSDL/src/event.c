2ec7b9971af012a84f94d7c14ead3dd2eff0c5cf
tracer-ics2023 <tracer@njuics.org>
2023-12-20 21:19:02 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:02 up  3:28,  1 user,  load average: 0.83, 0.51, 0.45

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,7 +15,10 @@ static char type = 0;
 static char name[10] = {0};
 static char buf[15] = {0};
 
+static char key_map[sizeof(keyname)];
+
 int SDL_PushEvent(SDL_Event *ev) {
+  assert(0);
   return 0;
 }
 
@@ -23,6 +26,7 @@ int SDL_PollEvent(SDL_Event *ev) {
   if (NDL_PollEvent(buf, sizeof(buf))) {
     // parsing the event
     assert(ev);
+    memset(key_map, 0, sizeof(key_map));
     sscanf(buf, "%c %s\n", &type, name);
     if (type == 'd')
       ev->type = SDL_KEYDOWN;
@@ -31,11 +35,12 @@ int SDL_PollEvent(SDL_Event *ev) {
     for (int i = 0; i < sizeof(keyname) / sizeof(char *); i++) {
       if (strcmp(name, keyname[i]) == 0) {
         ev->key.keysym.sym = i;
+        key_map[i] = (ev->type == SDL_KEYDOWN) ? 1 : 0;
         return 1;
       }
     }
     // parsing fail
-    return -1;
+    assert(0);
   }
   return 0;
 }
@@ -48,16 +53,28 @@ int SDL_WaitEvent(SDL_Event* event) {
     if((ret = SDL_PollEvent(event)) == 1)
       return 1;
     // fail
-    else if(ret == -1)
-      return 0;
     else continue;
   }
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
+  assert(0);
   return 0;
 }
 
+/*
+
+  Gets a snapshot of the current keyboard state. The current state is re‐
+  turn as a pointer to an array, the size of this array  is  stored  in
+  numkeys. The array is indexed by the SDLK_* symbols. A value of 1 means
+  the key is pressed and a value of 0 means its not. The pointer returned
+  is  a  pointer  to an internal SDL array and should not be freed by the
+  caller.
+
+*/
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+  if(numkeys != NULL) {
+    *numkeys = sizeof(key_map) / sizeof(char);
+  }
+  return key_map;
 }
