e970c8b731f07fad5cf4fe55a141123077abe00f
tracer-ics2023 <tracer@njuics.org>
2023-12-20 14:17:59 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:59 up  4:28,  1 user,  load average: 1.86, 1.29, 1.13

--- navy-apps/tests/eof-test/main.c
+++ navy-apps/tests/eof-test/main.c
@@ -6,7 +6,7 @@ int main() {
     char buf [10] = {0};
     while (!feof(fp)) {
         int str_in = fscanf(fp, "%s\n", buf);
-        printf("[%d]  eof:%d  offset:%d  size:%d  str:%s", times, feof(fp), ftell(fp), str_in, buf);
+        printf("[%d]  eof:%d  offset:%d  size:%d  str:%s\n", times, feof(fp), ftell(fp), str_in, buf);
         times++;
     }
     return 0;  
