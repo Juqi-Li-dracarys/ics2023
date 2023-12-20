3208da2912072e4de5cc99581bbc17469845018f
tracer-ysyx <tracer@ysyx.org>
2023-12-20 14:17:12 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:17:12 up  4:28,  1 user,  load average: 1.81, 1.17, 1.08

--- navy-apps/tests/eof-test/main.c
+++ navy-apps/tests/eof-test/main.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
-// ISA=native 不会出错
-// 目前问题定位在 libos 和 libc
-// 即两个 syscall
 int main() {
     FILE *fp = fopen("/share/files/test", "r");
     int times = 0;
     char buf [10] = {0};
-    while (!feof(fp) && times < 10)
-    {
-        // int size_in = fscanf(fp, "%s\n", buf);
-        int size_in = fread(buf, 1, 7, fp);
-        printf("[%d]  eof:%d  offset:%d  size:%d  str:%s", times, feof(fp), ftell(fp), size_in, buf);
+    while (!feof(fp)) {
+        int str_in = fscanf(fp, "%s\n", buf);
+        printf("[%d]  eof:%d  offset:%d  size:%d  str:%s", times, feof(fp), ftell(fp), str_in, buf);
         times++;
     }
-    // printf("hello world\n");
-    // printf("********************\n");
-    return 0;
-    
+    return 0;  
 }
