2891a190945a6b7b16e88a3049585aeb067add11
tracer-ysyx <tracer@ysyx.org>
2023-12-20 10:21:23 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:21:23 up  1:33,  1 user,  load average: 1.48, 1.11, 1.02

--- navy-apps/tests/eof-test/main.c
+++ navy-apps/tests/eof-test/main.c
@@ -7,11 +7,11 @@ int main() {
     FILE *fp = fopen("/share/files/test", "r");
     int times = 0;
     char buf [10] = {0};
-    while (!feof(fp) && times < 20)
+    while (!feof(fp) && times < 1)
     {
         // fscanf(fp, "%s\n", buf);       
         fread(buf, 7, 1, fp);
-        printf("[%d]  eof:%d  %s", times, feof(fp), buf);
+        printf("[%d]  eof:%d  str:%s  offset:%d", times, feof(fp), buf, ftell(fp));
         times++;
     }
     return 0;
