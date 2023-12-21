cdd8e64dc34c3e8e9780f65e2bc4b146be4d9767
tracer-ics2023 <tracer@njuics.org>
2023-12-21 13:04:38 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:04:38 up 4 min,  1 user,  load average: 0.98, 1.01, 0.47

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,11 +34,11 @@ void init_proc() {
   // naive_uload(NULL, "/bin/bmp-test");
   // naive_uload(NULL, "/bin/fixed-test");
   // naive_uload(NULL, "/bin/nslider");
-  // naive_uload(NULL, "/bin/menu");
+  naive_uload(NULL, "/bin/menu");
   // naive_uload(NULL, "/bin/nterm");
   // naive_uload(NULL, "/bin/bird");
   // naive_uload(NULL, "/bin/eof-test");
-  naive_uload(NULL, "/bin/pal");
+  // naive_uload(NULL, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
