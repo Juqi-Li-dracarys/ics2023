aaec86ed88b538f3ca0f8024dca996f2b52b28f9
tracer-ics2023 <tracer@njuics.org>
2024-01-14 18:10:57 +0800

    > sim RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:10:57 up  7:17,  1 user,  load average: 0.80, 0.44, 0.40

--- npc/naive_cpu/vsrc/ALU.sv
+++ npc/naive_cpu/vsrc/ALU.sv
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-13 20:39:08 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-14 18:01:05
+ * @Last Modified time: 2024-01-14 18:10:50
  */
 
 // ALU of the CPU
