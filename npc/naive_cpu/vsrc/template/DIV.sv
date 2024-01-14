e54250d34608b39daf9304581f9d57bdf4ad07b1
tracer-ics2023 <tracer@njuics.org>
2024-01-14 16:46:43 +0800

    > sim RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:46:43 up  5:52,  1 user,  load average: 0.76, 0.61, 0.51

--- npc/naive_cpu/vsrc/template/DIV.sv
+++ npc/naive_cpu/vsrc/template/DIV.sv
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-14 01:23:41 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-14 01:35:40
+ * @Last Modified time: 2024-01-14 16:44:39
  */
 
 /*
@@ -14,23 +14,23 @@
 // naive way of building a div
 
 module DIV #(parameter data_width = 32) (
-    input    [data_width-1 : 0]    data_a,
-    input    [data_width-1 : 0]    data_b,
-    input                          div_op,
-    output   [data_width-1 : 0]    result
+    input      [data_width-1 : 0]    data_a,
+    input      [data_width-1 : 0]    data_b,
+    input                            div_op,
+    output reg [data_width-1 : 0]    result
 );
 
     always_comb begin
         if(div_op) begin
             // Unsigned division
-            if(data_b)
+            if(data_b != 32'b0)
                 result = data_a / data_b;
             else
                 // Handle division by zero
                 result = {data_width{1'b0}};
         end 
         else begin
-            if(data_b) 
+            if(data_b != 32'b0) 
                 result = $signed(data_a) / $signed(data_b);
             else
                 result = {data_width{1'b0}};
