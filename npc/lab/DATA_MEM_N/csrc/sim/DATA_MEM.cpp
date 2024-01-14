c640fb73791765a35ce0b28db53dad6a78ed4059
tracer-ics2023 <tracer@njuics.org>
2024-01-14 20:42:41 +0800

    > sim RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:41 up  9:01,  1 user,  load average: 0.34, 0.35, 0.29

--- npc/lab/DATA_MEM_N/csrc/sim/DATA_MEM.cpp
+++ npc/lab/DATA_MEM_N/csrc/sim/DATA_MEM.cpp
@@ -59,7 +59,7 @@ int main(int argc, char** argv, char** env) {
 
         top->clk = 0;
         top->eval();
-        printf("ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("Before edge, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
         tfp->dump(contextp->time()); // dump wave
         contextp->timeInc(1);        // 推动仿真时间
 
@@ -70,12 +70,12 @@ int main(int argc, char** argv, char** env) {
         contextp->timeInc(1);        // 推动仿真时间
 
         top->eval();
-        printf("After edge 3, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("After edge 2, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
         tfp->dump(contextp->time()); // dump wave
         contextp->timeInc(1);        // 推动仿真时间
 
         i++;
-        // ramdom_addr++;
+        ramdom_addr++;
     }
 
 
