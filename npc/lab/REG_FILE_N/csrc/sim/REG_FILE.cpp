8f45d58ae40b94b726f3e9dd215f94a94e3b1cca
tracer-ysyx <tracer@ysyx.org>
2024-01-15 11:07:45 +0800

    > sim RTL ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:07:45 up  1:14,  1 user,  load average: 1.06, 1.11, 0.90

--- npc/lab/REG_FILE_N/csrc/sim/REG_FILE.cpp
+++ npc/lab/REG_FILE_N/csrc/sim/REG_FILE.cpp
@@ -5,32 +5,29 @@
 
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "VDATA_MEM.h"  
+#include "VREG_FILE.h"  
 
 // dpi-c
-#include "VDATA_MEM__Dpi.h"
+#include "VREG_FILE__Dpi.h"
 #include <verilated_dpi.h>
 
-// memory
-uint32_t mem  [5] = {0x0};
 
 // source
-uint32_t data [5] = {0x00000001, 0x80000000, 0x8100F081, 0x0011A01F, 0xFFFFFFFF};
+uint32_t data [5] = {0x12345678, 0x80000000, 0x8100F081, 0x0011A01F, 0xFFFFFFFF};
 
-extern "C" int vaddr_read (int addrs, int len) {
-    switch(len) {
-        case 1: return *(uint8_t  *)(mem + (uint32_t)addrs - 0x80000000);
-        case 2: return *(uint16_t *)(mem + (uint32_t)addrs - 0x80000000);
-        case 4: return *(uint32_t *)(mem + (uint32_t)addrs - 0x80000000);
-        default: return 0;
-    }
+
+uint32_t *cpu_gpr = NULL;
+
+extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
+    cpu_gpr = (uint32_t *)(((VerilatedDpiOpenVar*)r)->datap());
 }
+
 
 int main(int argc, char** argv, char** env) {
     // Verilator 初始化
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    VDATA_MEM* top = new VDATA_MEM{ contextp };
+    VREG_FILE* top = new VREG_FILE{ contextp };
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
     contextp->traceEverOn(true);            //打开追踪功能
@@ -40,42 +37,57 @@ int main(int argc, char** argv, char** env) {
 
     // 开始仿真
     // 注意时序问题
-    int ramdom_MemOp = 0;
     int ramdom_addr = 0x80000000;
     int i = 0;
 
+    top->clk = 0;
+    top->rst = 1;
+    top->inst = 0x80000000;
+    top->rf_busW = 0;
+    top->RegWr = 1;
+    top->eval();
+    tfp->dump(contextp->time()); // dump wave
+    contextp->timeInc(5);        // 推动仿真时间
+
+    top->clk = 1;
+    top->eval();
+    tfp->dump(contextp->time()); // dump wave
+    contextp->timeInc(5);        // 推动仿真时间
+
+    top->rst = 0;
+
     while ((!contextp->gotFinish()) && i < 5) {
 
-        top->WrEn = 1;
-        top->MemOp = ramdom_MemOp;
-        top->addr = ramdom_addr;
-        top->DataIn = data[i];
-
+        top->inst = data[i];
+        top->rf_busW = data[i];
         top->clk = 0;
         top->eval();
-        printf("Before edge, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("Before edge, busA = 0x%08x, busB = 0x%08x\n", top->rf_busA, top->rf_busB);
         tfp->dump(contextp->time()); // dump wave
         contextp->timeInc(1);        // 推动仿真时间
 
         // 上升沿后立即计算，这里 data_out 不会变化，因为有延迟，但其实已经写入
         top->clk = 1;
         top->eval();
-        printf("After edge 0, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("A0, busA = 0x%08x, busB = 0x%08x\n", top->rf_busA, top->rf_busB);
         tfp->dump(contextp->time()); // dump wave
-        contextp->timeInc(1);        // 推动仿真时间
+        contextp->timeInc(2);        // 推动仿真时间
 
         // 1ps 后 data_out 变化
         top->eval();
-        printf("After edge 1, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("A1 edge, busA = 0x%08x, busB = 0x%08x\n", top->rf_busA, top->rf_busB);
         tfp->dump(contextp->time()); // dump wave
-        contextp->timeInc(1);        // 推动仿真时间
+        contextp->timeInc(3);        // 推动仿真时间
 
         i++;
         ramdom_addr++;
     }
 
-    for(i = 0; i < 5; i++) {
-        printf("0x%08x\n", mem[i]);
+    for(i = 0; i < 16; i++) {
+        printf("0x%08x\t", cpu_gpr[i]);
+        if(i % 3 == 0) {
+            putchar('\n');
+        }
     }
 
     delete top;
