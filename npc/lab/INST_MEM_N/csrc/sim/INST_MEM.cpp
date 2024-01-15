eab4e556f5de029d536a61378d53cad62e3b97d7
tracer-ics2023 <tracer@njuics.org>
2024-01-15 09:54:40 +0800

    > sim RTL 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:54:40 up 20 min,  1 user,  load average: 0.69, 0.55, 0.47

--- npc/lab/INST_MEM_N/csrc/sim/INST_MEM.cpp
+++ npc/lab/INST_MEM_N/csrc/sim/INST_MEM.cpp
@@ -5,19 +5,16 @@
 
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "VDATA_MEM.h"  
+#include "VINST_MEM.h"  
 
 // dpi-c
-#include "VDATA_MEM__Dpi.h"
+#include "VINST_MEM__Dpi.h"
 #include <verilated_dpi.h>
 
-// memory
-uint32_t mem  [5] = {0x0};
 
-// source
-uint32_t data [5] = {0x00000001, 0x80000000, 0x8100F081, 0x0011A01F, 0xFFFFFFFF};
+uint32_t mem [5] = {0x00000001, 0x80000000, 0x8100F081, 0x0011A01F, 0xFFFFFFFF};
 
-extern "C" int vaddr_read (int addrs, int len) {
+extern "C" int vaddr_ifetch (int addrs, int len) {
     switch(len) {
         case 1: return *(uint8_t  *)(mem + (uint32_t)addrs - 0x80000000);
         case 2: return *(uint16_t *)(mem + (uint32_t)addrs - 0x80000000);
@@ -30,7 +27,7 @@ int main(int argc, char** argv, char** env) {
     // Verilator 初始化
     VerilatedContext* contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
-    VDATA_MEM* top = new VDATA_MEM{ contextp };
+    VINST_MEM* top = new VINST_MEM{ contextp };
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
     contextp->traceEverOn(true);            //打开追踪功能
@@ -44,38 +41,39 @@ int main(int argc, char** argv, char** env) {
     int ramdom_addr = 0x80000000;
     int i = 0;
 
-    while ((!contextp->gotFinish()) && i < 5) {
 
-        top->WrEn = 1;
-        top->MemOp = ramdom_MemOp;
-        top->addr = ramdom_addr;
-        top->DataIn = data[i];
+    top->clk = 0;
+    top->rst = 1;
+    top->next_addr = ramdom_addr;
+    top->eval();
+    tfp->dump(contextp->time()); // dump wave
+    contextp->timeInc(5);        // 推动仿真时间
 
+    top->clk = 1;
+    top->eval();
+    tfp->dump(contextp->time()); // dump wave
+    contextp->timeInc(5);        // 推动仿真时间
+
+    top->rst = 0;
+
+   while ((!contextp->gotFinish()) && i < 5) {
+
+        top->next_addr = ramdom_addr;
         top->clk = 0;
         top->eval();
-        printf("Before edge, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("Before edge, inst = 0x%08x\n", top->inst);
         tfp->dump(contextp->time()); // dump wave
         contextp->timeInc(1);        // 推动仿真时间
 
         // 上升沿后立即计算，这里 data_out 不会变化，因为有延迟，但其实已经写入
         top->clk = 1;
         top->eval();
-        printf("After edge 0, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
-        tfp->dump(contextp->time()); // dump wave
-        contextp->timeInc(1);        // 推动仿真时间
-
-        // 1ps 后 data_out 变化
-        top->eval();
-        printf("After edge 1, ramdom_MemOp = 0x%08x, ramdom_addr = 0x%08x, result = 0x%08x\n", ramdom_MemOp, ramdom_addr, top->DataOut);
+        printf("After edge, inst = 0x%08x\n", top->inst);
         tfp->dump(contextp->time()); // dump wave
         contextp->timeInc(1);        // 推动仿真时间
 
         i++;
         ramdom_addr++;
-    }
-
-    for(i = 0; i < 5; i++) {
-        printf("0x%08x\n", mem[i]);
     }
 
     delete top;
