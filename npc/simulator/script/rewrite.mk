1433386abc6e1ee228de41084cd45af8f0e20779
tracer-ics2023 <tracer@njuics.org>
2024-01-16 21:03:29 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:29 up  1:11,  1 user,  load average: 1.40, 1.11, 0.83

--- npc/simulator/script/rewrite.mk
+++ npc/simulator/script/rewrite.mk
@@ -11,9 +11,9 @@ default: VCPU_TOP
 
 include VCPU_TOP.mk
 
-CXXFLAGS += -MMD -O3 -std=c++14 -fno-exceptions -fPIE -Wno-unused-result
+# CXXFLAGS += -MMD -O3 -std=c++14 -fno-exceptions -fPIE -Wno-unused-result
 CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIC -DDEVICE -D__GUEST_ISA__=$(ISA)
-LDFLAGS += -O3 -rdynamic -shared -fPIC
+# LDFLAGS += -O3 -rdynamic -shared -fPIC
 LIBS += $(shell llvm-config-11 --libs)
 LIBS += -lreadline -ldl -pie -lSDL2
-LINK := g++
+# LINK := g++
