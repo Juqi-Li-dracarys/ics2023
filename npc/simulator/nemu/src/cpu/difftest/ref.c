02469855275553c23784ce2b6edd1e3c44852c88
tracer-ics2023 <tracer@njuics.org>
2024-01-17 23:15:07 +0800

    > GDB NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:15:07 up 12:29,  1 user,  load average: 0.44, 0.38, 0.46

--- npc/simulator/nemu/src/cpu/difftest/ref.c
+++ npc/simulator/nemu/src/cpu/difftest/ref.c
@@ -28,6 +28,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_DUT) {
+    printf("0x%08x\n", cpu.pc);
     memcpy(dut, &cpu, DIFFTEST_REG_SIZE);
   } else if (direction == DIFFTEST_TO_REF) {
     memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
@@ -35,9 +36,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  printf("0x%08x\n", cpu.pc);
   cpu_exec(n);
-  printf("0x%08x\n", cpu.pc);
 }
 
 __EXPORT void difftest_init(int port) {
