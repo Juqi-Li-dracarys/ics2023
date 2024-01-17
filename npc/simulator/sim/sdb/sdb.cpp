1dc60fb22ebd9e73ced752779a364063730530de
tracer-ysyx <tracer@ysyx.org>
2024-01-17 23:39:13 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:39:13 up 12:53,  1 user,  load average: 0.79, 0.62, 0.64

--- npc/simulator/sim/sdb/sdb.cpp
+++ npc/simulator/sim/sdb/sdb.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-17 18:53:49 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-17 23:35:02
+ * @Last Modified time: 2024-01-17 23:38:47
  */
 
 #include <common.h>
@@ -26,7 +26,7 @@ static char* rl_gets() {
     line_read = NULL;
   }
 
-  line_read = readline("(NPC_simulator) ");
+  line_read = readline("(simulator) ");
 
   if (line_read && *line_read) {
     add_history(line_read);
