49bc412b2e3370d8c1859ca3716af843681e7b59
tracer-ics2023 <tracer@njuics.org>
2024-01-17 22:11:42 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:11:42 up 11:25,  1 user,  load average: 0.85, 0.45, 0.45

--- npc/simulator/sim/difftest/difftest.cpp
+++ npc/simulator/sim/difftest/difftest.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-16 11:00:24 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-17 22:10:02
+ * @Last Modified time: 2024-01-17 22:11:35
  */
 
 #include <dlfcn.h>
@@ -148,6 +148,6 @@ void difftest_step() {
   difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
   // difftest_memcpy(CONFIG_MBASE, ref_pmem, CONFIG_MSIZE, DIFFTEST_TO_DUT);
   checkregs(&ref_r, sim_cpu.pc);
-  difftest_exec(1);
+  // difftest_exec(1);
   // checkmem(ref_pmem, sim_cpu.pc);
 }
