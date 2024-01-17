ae7af553d3cc6e6640d41911ff5b4869a22d68f1
tracer-ics2023 <tracer@njuics.org>
2024-01-17 23:02:52 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:02:52 up 12:17,  1 user,  load average: 0.89, 0.72, 0.63

--- npc/simulator/nemu/src/cpu/difftest/ref.c
+++ npc/simulator/nemu/src/cpu/difftest/ref.c
@@ -36,6 +36,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 
 __EXPORT void difftest_exec(uint64_t n) {
   cpu_exec(n);
+  printf("0x%08x\n", cpu.pc);
 }
 
 __EXPORT void difftest_init(int port) {
