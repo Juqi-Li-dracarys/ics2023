a15857ede0fe05728229fa44a08d7fa68681e124
tracer-ics2023 <tracer@njuics.org>
2024-01-17 22:52:03 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:52:03 up 12:06,  1 user,  load average: 1.08, 0.79, 0.61

--- npc/simulator/sim/cpu/cpu_exe.cpp
+++ npc/simulator/sim/cpu/cpu_exe.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-17 09:39:10 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-17 22:39:31
+ * @Last Modified time: 2024-01-17 22:51:52
  */
 
 #include <bits/stdc++.h>
