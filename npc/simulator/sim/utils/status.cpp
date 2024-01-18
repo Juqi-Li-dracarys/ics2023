e45273c520bc8c44b60a5cfd4e771e5d8fd2a1fd
tracer-ics2023 <tracer@njuics.org>
2024-01-18 11:45:23 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:23 up  3:38,  1 user,  load average: 1.25, 1.17, 0.92

--- npc/simulator/sim/utils/status.cpp
+++ npc/simulator/sim/utils/status.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-17 21:47:12 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-18 10:54:28
+ * @Last Modified time: 2024-01-18 11:05:11
  */
 
 #include <common.h>
@@ -13,7 +13,6 @@ bool is_exit_status_bad() {
     bool good = (sim_state.state == SIM_END && sim_state.halt_ret == 0) ||
     (sim_state.state == SIM_QUIT);
     printf(ANSI_FG_YELLOW "Simulator exit\n" ANSI_NONE);
-    printf("%d\n", sim_state.state);
     return !good;
 }
 
