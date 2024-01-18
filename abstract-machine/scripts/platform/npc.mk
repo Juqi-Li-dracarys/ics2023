cb5dc1642f4db8d8c311fee832b454fef3863f33
tracer-ics2023 <tracer@njuics.org>
2024-01-18 10:35:36 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:35:36 up  2:29,  1 user,  load average: 1.36, 0.69, 0.52

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -31,4 +31,4 @@ image: $(IMAGE).elf
 
 run: image
 	@echo + Load "->" $(IMAGE).bin
-	$(MAKE) -C $(SIMULATOR_HOME) run ARGS="$(NPC_FLAGS)" IMG="$(IMAGE).bin"
+	$(MAKE) -C $(SIMULATOR_HOME) ARGS="$(NPC_FLAGS)" IMG="$(IMAGE).bin" run
