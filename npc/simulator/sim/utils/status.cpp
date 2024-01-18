90e750d7e5ab42a1897792037114e7e76e7c0fe8
tracer-ysyx <tracer@ysyx.org>
2024-01-18 10:52:09 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:52:09 up  2:45,  1 user,  load average: 2.00, 1.52, 1.04

--- npc/simulator/sim/utils/status.cpp
+++ npc/simulator/sim/utils/status.cpp
@@ -13,6 +13,6 @@ bool is_exit_status_bad() {
     bool good = (sim_state.state == SIM_END && sim_state.halt_ret == 0) ||
     (sim_state.state == SIM_QUIT);
     printf(ANSI_FG_YELLOW "Simulator exit\n" ANSI_NONE);
-    return true;
+    return false;
 }
 
