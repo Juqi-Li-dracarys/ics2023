7c070c67806304abe53ba179c847066721de2db5
tracer-ysyx <tracer@ysyx.org>
2024-01-18 08:31:03 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:31:03 up 42 min,  1 user,  load average: 0.57, 0.59, 0.56

--- npc/simulator/sim/sdb/monitor.cpp
+++ npc/simulator/sim/sdb/monitor.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-16 16:33:49 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-18 08:30:33
+ * @Last Modified time: 2024-01-18 08:31:00
  */
 
 #include <common.h>
@@ -20,7 +20,7 @@ static void welcome() {
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
   printf("For help, type \"help\", as an EE rat @NJU, please always remember to RTFM:\n");
-  printf("\n%s\n", isa_logo);
+  printf("%s", isa_logo);
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NPC Simulator! \n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
 }
