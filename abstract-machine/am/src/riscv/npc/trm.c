67b5aeab5f71f5a84008bff5d99bf37f904fa732
tracer-ics2023 <tracer@njuics.org>
2024-01-18 21:03:29 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:29 up  1:10,  1 user,  load average: 0.57, 0.69, 0.54

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,24 +1,28 @@
-#include <am.h>
-#include <klib-macros.h>
+/*
+ * @Author: Juqi Li @ NJU 
+ * @Date: 2024-01-18 20:54:49 
+ * @Last Modified by: Juqi Li @ NJU
+ * @Last Modified time: 2024-01-18 20:57:31
+ */
 
+#include <am.h>
+#include <npc.h>
 
 extern char _heap_start;
 int main(const char *args);
-
-extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
-#define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
+
 static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
+  outb(SERIAL_PORT, ch);
 }
 
-// 内联汇编，结束程序
+// 内联汇编，结束 npc 
 void halt(int code) {
   asm volatile(
     "mv a0, %0\n\t"
--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,6 +15,8 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
 						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
+CFLAGS += -I$(AM_HOME)/am/src/riscv/npc/include
+
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
 NPC_FLAGS += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
