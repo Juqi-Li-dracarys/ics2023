3c99acc1912018f6337a608b3512084b2bf6ddaa
tracer-ics2023 <tracer@njuics.org>
2024-01-18 11:01:36 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:01:36 up  2:55,  1 user,  load average: 0.91, 0.98, 1.00

--- npc/simulator/sim/sdb/sdb.cpp
+++ npc/simulator/sim/sdb/sdb.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-17 18:53:49 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-18 10:56:56
+ * @Last Modified time: 2024-01-18 11:01:28
  */
 
 #include <common.h>
@@ -42,7 +42,7 @@ static int cmd_c(char *args) {
 
 static int cmd_q(char *args) {
   // Change the flag of nemu_state
-  sim_state.state == SIM_QUIT;
+  sim_state.state = SIM_QUIT;
 #ifdef CONFIG_ITRACE
   destroy_ring_buffer(ring_head);
 #endif
