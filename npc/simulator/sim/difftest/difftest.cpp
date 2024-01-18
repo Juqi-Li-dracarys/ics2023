5eee2eef01969e64c7e8c11720f1295ef5a9d201
tracer-ysyx <tracer@ysyx.org>
2024-01-18 10:39:09 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:39:09 up  2:32,  1 user,  load average: 0.72, 0.71, 0.56

--- npc/simulator/sim/difftest/difftest.cpp
+++ npc/simulator/sim/difftest/difftest.cpp
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-16 11:00:24 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-01-17 23:28:48
+ * @Last Modified time: 2024-01-18 10:38:43
  */
 
 #include <dlfcn.h>
@@ -147,9 +147,11 @@ void difftest_step(bool interrupt) {
   REF_CPU_state ref_r;
   difftest_exec(1);
   difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  // difftest_memcpy(CONFIG_MBASE, ref_pmem, CONFIG_MSIZE, DIFFTEST_TO_DUT);
-  if(!interrupt) checkregs(&ref_r, sim_cpu.pc);
-  // checkmem(ref_pmem, sim_cpu.pc);
+  difftest_memcpy(CONFIG_MBASE, ref_pmem, CONFIG_MSIZE, DIFFTEST_TO_DUT);
+  if(!interrupt) {
+    checkregs(&ref_r, sim_cpu.pc);
+    checkmem(ref_pmem, sim_cpu.pc);
+  }
 }
 
 #else
