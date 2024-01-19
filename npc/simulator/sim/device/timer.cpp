89237285e55fb043a4930893346c18e6f8655324
tracer-ics2023 <tracer@njuics.org>
2024-01-19 21:20:01 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:20:01 up  3:26,  1 user,  load average: 1.64, 1.01, 0.86

--- npc/simulator/sim/device/timer.cpp
+++ npc/simulator/sim/device/timer.cpp
@@ -6,17 +6,26 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(!is_write);
-  uint64_t us = get_time();
-  struct tm* rtc = get_time_tm();
-  rtc_port_base[0] = (uint32_t)us;
-  rtc_port_base[1] = us >> 32;
-  rtc_port_base[2] = rtc->tm_sec;
-  rtc_port_base[3] = rtc->tm_min;
-  rtc_port_base[4] = rtc->tm_hour;
-  rtc_port_base[5] = rtc->tm_mday;
-  rtc_port_base[6] = rtc->tm_mon + 1;
-  rtc_port_base[7] = rtc->tm_year + 1900;
+  
+  // assert(!is_write);
+  // uint64_t us = get_time();
+  // struct tm* rtc = get_time_tm();
+  // rtc_port_base[0] = (uint32_t)us;
+  // rtc_port_base[1] = us >> 32;
+  // rtc_port_base[2] = rtc->tm_sec;
+  // rtc_port_base[3] = rtc->tm_min;
+  // rtc_port_base[4] = rtc->tm_hour;
+  // rtc_port_base[5] = rtc->tm_mday;
+  // rtc_port_base[6] = rtc->tm_mon + 1;
+  // rtc_port_base[7] = rtc->tm_year + 1900;
+
+  assert(offset == 0 || offset == 4);
+  //注意，这里 offset = 4 读高32位时不会更新（已经修正bug）
+  if (!is_write && offset == 0) {
+    uint64_t us = get_time();
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = us >> 32;
+  }
 }
 
 void init_timer() {
