8ff536d17e53c6245279c01d71905416ec1519b6
tracer-ysyx <tracer@ysyx.org>
2024-01-20 10:37:21 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:21 up  2:05,  1 user,  load average: 3.02, 1.55, 1.13

--- npc/simulator/sim/device/timer.cpp
+++ npc/simulator/sim/device/timer.cpp
@@ -6,13 +6,10 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
+  // to avoid error
   // assert(offset == 0 || offset == 4);
-  if(offset != 0 && offset != 4) {
-    printf("\nI have been fuck:%d\n", offset);
-    sim_state.state = SIM_ABORT;
-  }
-
-  //注意，这里 offset = 4 读高32位时不会更新（已经修正bug）
+  // 注意，这里 offset = 4 读高32位时不会更新
+  // 防止数据重复更新
   if (!is_write && offset == 0) {
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
