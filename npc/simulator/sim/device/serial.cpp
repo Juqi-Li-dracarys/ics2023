7f9c0eef47d2318f6232f2f51c24578f33e577b3
tracer-ics2023 <tracer@njuics.org>
2024-01-20 10:37:21 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:21 up  2:05,  1 user,  load average: 3.02, 1.55, 1.13

--- npc/simulator/sim/device/serial.cpp
+++ npc/simulator/sim/device/serial.cpp
@@ -12,16 +12,8 @@ static void serial_putc(char ch) {
 }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(len == 1);
-  switch (offset) {
-    /* We bind the serial port with the host stderr in NEMU. */
-    case CH_OFFSET:
-      if (is_write) serial_putc(serial_base[0]);
-      // just comment it, to avoid error
-      // else panic("serial do not support read");
-      break;
-    default: panic("do not support offset = %d", offset);
-  }
+  if (is_write) serial_putc(serial_base[0]);
+  return;
 }
 
 void init_serial() {
