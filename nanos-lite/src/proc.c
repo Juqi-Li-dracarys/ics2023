bb64ae0820272f558eef7d0eea75a358f76e76f5
tracer-ics2023 <tracer@njuics.org>
2024-01-20 22:52:36 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:52:36 up  3:59,  1 user,  load average: 0.67, 0.60, 0.51

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,10 @@ void hello_fun(void *arg) {
     for (int volatile i = 0; i < 100000; i++) ;
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
     j ++;
-    yield();
+    if(j < 10)
+      yield();
+    else
+      return;
   }
 }
 
