be7a3e4e9d1449853caae8c8b13de26195ac744d
tracer-ysyx <tracer@ysyx.org>
2024-01-21 15:21:58 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:58 up  6:23,  1 user,  load average: 0.46, 0.79, 1.00

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 
 void call_main(uintptr_t *args) {
+  
   // int argc = (int)args[0];
   // int envc = (int)args[argc + 2];
   // char **argv = (char **)(args + 1);
--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  // printf("Hello\n");
-  // write(1, "Hello World!\n", 13);
-  // int i = 0;
-  // volatile int j = 0;
+  printf("Hello\n");
+  write(1, "Hello World!\n", 13);
+  int i = 0;
+  volatile int j = 0;
 
-  // while (i < 5) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
-  // char buf[15] = {0};
-  // sprintf(buf, "%d\n", 0x80000000);
-  // printf("%s", buf);
-  printf("%d\n", argc);
-  printf("%s\n", argv[0]);
+  while (i < 5) {
+    j ++;
+    if (j == 10000) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      j = 0;
+    }
+  }
+  char buf[15] = {0};
+  sprintf(buf, "%d\n", 0x80000000);
+  printf("%s", buf);
+  // printf("%d\n", argc);
+  // printf("%s\n", argv[0]);
   return 0;
 }
