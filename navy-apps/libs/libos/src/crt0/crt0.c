f783b27137c9c6981b96d0385d1ca8c1d8194a29
tracer-ics2023 <tracer@njuics.org>
2024-01-21 13:23:22 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:22 up  4:44,  1 user,  load average: 1.14, 1.05, 0.92

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -8,14 +8,16 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 
 void call_main(uintptr_t *args) {
+
   int argc = (int)args[0];
   int envc = (int)args[argc + 2];
   char **argv = (char **)(args + 1);
   char **envp = (char **)(args + argc + 3);
 
   assert(argc == 1 && envc == 1);
-  assert(strcmp("hello_ar", argv[0]) == 0);
-
+  assert(strcmp("hello_arg", argv[0]) == 0);
+  assert(0);
+  
   // int argc = 0;
   // char **argv = NULL;
   // char **envp = NULL;
