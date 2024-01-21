3ea851480540857e07ec9d0519b513eab908d7c9
tracer-ics2023 <tracer@njuics.org>
2024-01-22 00:00:50 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:00:50 up 14:14,  1 user,  load average: 1.47, 0.87, 0.72

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -34,12 +34,12 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
   for (i = 0; i < LENGTH(segments); i ++) {
     void *va = segments[i].start;
     for (; va < segments[i].end; va += PGSIZE) {
-      printf("OK\n");
+      printf("OK1\n");
       map(&kas, va, va, 0);
     }
   }
   
-  printf("OK\n");
+  printf("OK2\n");
   set_satp(kas.ptr);
   vme_enable = 1;
 
@@ -69,24 +69,64 @@ void __am_switch(Context *c) {
 }
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-  // 各个地址提取
-  uint32_t PPN = PA_PPN((uint32_t)pa);
-  uint32_t VPN_1 = VA_VPN_1((uint32_t)va);
-  uint32_t VPN_2 = VA_VPN_2((uint32_t)va);
+
+  // // 各个地址提取
+  // uint32_t PPN = PA_PPN((uint32_t)pa);
+  // uint32_t VPN_1 = VA_VPN_1((uint32_t)va);
+  // uint32_t VPN_2 = VA_VPN_2((uint32_t)va);
  
-  // 一级页表的目标位置
-  PTE *VPN_1_TARGET = as->ptr + VPN_1;
-  PTE *VPN_2_BASE = NULL;
-  // 如果一级页表中的页表项的地址为空，则创建页表项
-  if (!(*VPN_1_TARGET)) { 
-    // 设置二级页表的基地址, 指向一个物理页面，专门存储页表
-    *VPN_1_TARGET = (PTE)pgalloc_usr(PGSIZE);
+  // // 一级页表的目标位置
+  // PTE *VPN_1_TARGET = as->ptr + VPN_1;
+  // PTE *VPN_2_BASE = NULL;
+  // // 如果一级页表中的页表项的地址为空，则创建页表项
+  // if (!(*VPN_1_TARGET)) { 
+  //   // 设置二级页表的基地址, 指向一个物理页面，专门存储页表
+  //   *VPN_1_TARGET = (PTE)pgalloc_usr(PGSIZE);
+  // }
+  // // 第二页表基地址和目标地址
+  // VPN_2_BASE = (PTE *)(*VPN_1_TARGET);
+  // PTE *VPN_2_TAGET = VPN_2_BASE + VPN_2;
+  // // 将物理页号填写到二级页表的页表项中，低 12 位是标志位
+  // *VPN_2_TAGET = (PPN << 12) | 0xF;
+
+
+
+
+#define PTE_PPN 0xFFFFF000
+  uintptr_t va_trans = (uintptr_t) va;
+  uintptr_t pa_trans = (uintptr_t) pa;
+ 
+  assert(PA_OFFSET(pa_trans) == 0);
+  assert(VA_OFFSET(va_trans) == 0);
+ 
+  //提取虚拟地址的二级页号和一级页号，以及物理地址的物理页号
+  uint32_t ppn = PA_PPN(pa_trans);
+  uint32_t vpn_1 = VA_VPN_1(va_trans);
+  uint32_t vpn_2 = VA_VPN_2(va_trans);
+ 
+  //获取地址空间的页表基址和一级页表的目标位置
+  PTE * page_dir_base = (PTE *) as->ptr;
+  PTE * page_dir_target = page_dir_base + vpn_1;
+  
+  //如果一级页表中的页表项的地址(二级页表的基地址)为空，创建并填写页表项
+  if (*page_dir_target == 0) { 
+    //通过 pgalloc_usr 分配一页物理内存，作为二级页表的基地址
+    PTE * page_table_base = (PTE *) pgalloc_usr(PGSIZE);
+    //将这个基地址填写到一级页表的页表项中，同时设置 PTE_V 表示这个页表项是有效的。
+    *page_dir_target = ((PTE) page_table_base) | PTE_V;
+    //计算在二级页表中的页表项的地址
+    PTE * page_table_target = page_table_base + vpn_2;
+    //将物理页号 ppn 左移 12 位，即去掉低 12 位的偏移，与权限标志 PTE_V | PTE_R | PTE_W | PTE_X 组合，填写到二级页表的页表项中。
+    *page_table_target = (ppn << 12) | PTE_V | PTE_R | PTE_W | PTE_X;
+  } else {
+    //取得一级页表项的内容，然后 & PTE_PPN 通过按位与操作提取出页表的基地址，提取高20位，低 12 位为零
+    PTE * page_table_base = (PTE *) ((*page_dir_target) & PTE_PPN);
+    //通过加上 vpn_0 计算得到在二级页表中的目标项的地址
+    PTE * page_table_target = page_table_base + vpn_2;
+    //将物理页号 ppn 左移 12 位，即去掉低 12 位的偏移，与权限标志 PTE_V | PTE_R | PTE_W | PTE_X 组合，填写到二级页表的目标项中。
+    *page_table_target = (ppn << 12) | PTE_V | PTE_R | PTE_W | PTE_X;
   }
-  // 第二页表基地址和目标地址
-  VPN_2_BASE = (PTE *)(*VPN_1_TARGET);
-  PTE *VPN_2_TAGET = VPN_2_BASE + VPN_2;
-  // 将物理页号填写到二级页表的页表项中，低 12 位是标志位
-  *VPN_2_TAGET = (PPN << 12) | 0xF;
+
 }
 
 
