80e39dd92dacc566580239e1a576519f9e8bc6f7
tracer-ics2023 <tracer@njuics.org>
2024-01-21 15:44:53 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:53 up  6:46,  1 user,  load average: 0.94, 0.75, 0.90

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[], char *envp[]) {
   // printf("Hello\n");
   // write(1, "Hello World!\n", 13);
   // int i = 0;
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
   // sprintf(buf, "%d\n", 0x80000000);
   // printf("%s", buf);
 
-  
+
   printf("%d\n", argc);
   printf("%s\n", argv[0]);
   return 0;
