ceee49fd6bc292203e662d50d3035a3314a4f003
tracer-ics2023 <tracer@njuics.org>
2024-01-26 18:24:40 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:24:40 up  5:37,  1 user,  load average: 1.13, 0.78, 0.71

--- npc/naive_cpu/vsrc/REG_FILE.sv
+++ npc/naive_cpu/vsrc/REG_FILE.sv
@@ -7,7 +7,8 @@
 
 // register file for RV32E
 
-module REG_FILE #(parameter reg_num = 16, reg_width = 4)(
+
+module REG_FILE #(parameter gpr_reg_num = 5'd16, csr_reg_num = 5'd4, gpr_reg_width = 4, csr_reg_width = 2)(
     input             clk,                      // clk for write
     input             rst,
     input   [31 : 0]  inst,
@@ -25,36 +26,52 @@ module REG_FILE #(parameter reg_num = 16, reg_width = 4)(
     assign rs2 = inst[24 : 20];
     assign rd = inst[11 : 7];
 
+
     // DIP-C in verilog
     import "DPI-C" function void set_gpr_ptr(input logic [31 : 0] a []);
+    import "DPI-C" function void set_csr_ptr(input logic [31 : 0] a, 
+    input logic [31 : 0] b, input logic [31 : 0] c, input logic [31 : 0] d);
 
-    reg  [31 : 0]   reg_file [0 : reg_num-1];
+
+    // mstatus mtvec mepc mcause;
+    reg  [31 : 0]   gpr_reg  [0 : gpr_reg_num - 1];
+    reg  [31 : 0]   csr_reg  [0 : csr_reg_num - 1];
+
 
     // set the ptr to register
     initial begin
-        set_gpr_ptr(reg_file);
+        set_gpr_ptr(gpr_reg);
+        set_csr_ptr(csr_reg[0], csr_reg[1], csr_reg[2], csr_reg[3]);
     end
 
-    // write the register in the next posedge
+
+    // write the reg in the next posedge
     integer i;
     always_ff @(posedge clk) begin
         // should not write $0
-        if(RegWr && !rst && rd != 5'h0 && rd < reg_num) begin
-            reg_file[rd[reg_width-1 : 0]] <= rf_busW;
-        end
-        else if(rst) begin
-            for(i = 0; i < reg_num; i = i + 1) begin
-                reg_file[i] <= 32'h0;
+        if(rst) begin
+            for(i = 0; i < gpr_reg_num; i = i + 1) begin
+                gpr_reg[i] <= 32'h0;
             end
+            for(i = 1; i < csr_reg_num; i = i + 1) begin
+                csr_reg[i] <= 32'h0;
+            end
+            csr_reg[0] <= 32'h1800;
+        end
+        else if(RegWr) begin
+            if(rd < gpr_reg_num)
+                gpr_reg[rd[3 : 0]] <= rf_busW;
+            else 
+                csr_reg[{rd - gpr_reg_num}[1 : 0]] <= rf_busW;
         end
     end
 
     // read register
-    assign rf_busA = reg_file[rs1[reg_width-1 : 0]];
-    assign rf_busB = reg_file[rs2[reg_width-1 : 0]];
+    assign rf_busA = rs1 < gpr_reg_num ? gpr_reg[rs1[3 : 0]] : csr_reg[{rs1 - gpr_reg_num}[1 : 0]];
+    assign rf_busB = rs2 < gpr_reg_num ? gpr_reg[rs2[3 : 0]] : csr_reg[{rs2 - gpr_reg_num}[1 : 0]];
 
     // Error sinal
-    assign reg_signal = RegWr && (rd >= reg_num);
+    assign reg_signal = RegWr && (rd >= gpr_reg_num + csr_reg_num);
 
 endmodule
 
