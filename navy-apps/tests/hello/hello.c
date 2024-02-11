048c87030d6587d28f625afde8aa588cad9c5024
tracer-ysyx <tracer@ysyx.org>
2024-02-12 00:11:53 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-17-generic #17~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jan 16 14:32:32 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:11:53 up 10:55,  1 user,  load average: 1.13, 1.27, 1.10

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -4,23 +4,23 @@
 int main(int argc, char *argv[], char *envp[]) {
   // printf("Hello\n");
   // write(1, "Hello World!\n", 13);
-  // int i = 0;
-  // volatile int j = 0;
 
-  // while (i < 5) {
-  //   j ++;
-  //   if (j == 10000) {
-  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-  //     j = 0;
-  //   }
-  // }
+  int i = 0;
+  volatile int j = 0;
+  while (1) {
+    j ++;
+    if (j == 10000) {
+      j = 0;
+      printf("Hello World from Navy-apps for the %dth time in APP!\n", i ++);
+    }
+  }
   // char buf[15] = {0};
   // sprintf(buf, "%d\n", 0x80000000);
   // printf("%s", buf);
 
 
-  printf("0x%08x\n", argc);
-  printf("%s\n", argv[0]);
-  printf("%s\n", envp[0]);
+  // printf("0x%08x\n", argc);
+  // printf("%s\n", argv[0]);
+  // printf("%s\n", envp[0]);
   return 0;
 }
