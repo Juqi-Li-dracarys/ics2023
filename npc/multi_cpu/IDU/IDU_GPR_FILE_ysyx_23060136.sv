5600ae9d4c21833486a5ea2f5ca8f7ea6b68443d
tracer-ics2023 <tracer@njuics.org>
2024-02-20 09:44:29 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:44:29 up 18 min,  1 user,  load average: 0.39, 0.42, 0.38

---
+++ npc/multi_cpu/IDU/IDU_GPR_FILE_ysyx_23060136.sv
@@ -0,0 +1,88 @@
+/*
+ * @Author: Juqi Li @ NJU 
+ * @Date: 2024-02-19 17:45:55 
+ * @Last Modified by: Juqi Li @ NJU
+ * @Last Modified time: 2024-02-19 18:17:52
+ */
+
+
+// gpr file for RV32E
+
+ `include "IDU_DEFINES_ysyx23060136.sv"
+
+// ===========================================================================
+module IDU_GPR_FILE_ysyx_23060136 (
+
+        input             clk,                      // clk for write
+        input             rst,
+        input   [4 : 0]   rs1,
+        input   [4 : 0]   rs2,
+        input   [4 : 0]   rd,
+        input             RegWr,                    // write gpr reg enable
+        input   [31 : 0]  rf_busW,                  // write gpr data
+        output  [31 : 0]  rf_busA, rf_busB          // gpr read result
+    );
+
+    // DIP-C in verilog
+    import "DPI-C" function void set_gpr_ptr(input logic [31 : 0] a []);
+
+    // set the ptr to register
+    initial begin
+        set_gpr_ptr(gpr_reg);
+    end
+
+    logic  [31 : 0]  gpr_reg      [15 : 0];
+    // write enable
+    logic            w_e          [15 : 0];
+    // read enable
+    logic            r_e_1        [15 : 0];
+    logic            r_e_2        [15 : 0];
+    // data_out(temp data)
+    logic  [31 : 0]  data_out_1   [15 : 0] /*verilator split_var*/;
+    logic  [31 : 0]  data_out_2   [15 : 0] /*verilator split_var*/; 
+    
+    
+    integer i;
+    always_ff @(posedge clk) begin
+        // should not write $0
+        if(rst) begin
+            for(i = 0; i < 16; i = i + 1) begin
+                gpr_reg[i] <= 32'h0;
+            end
+        end
+        else begin
+            for(i = 0; i < 16; i = i + 1) begin
+                gpr_reg[i] <= w_e[i] ? rf_busW : gpr_reg[i];
+            end
+        end
+    end
+
+    genvar j;
+    generate
+        for (j = 0; j < 16; j = j + 1) begin
+            if (j == 0) begin
+                assign w_e[j] = 1'b0;
+            end
+            else begin
+                assign w_e[j] = RegWr & (rd == j);
+            end
+            assign r_e_1[j] = (rs1 == j);
+            assign r_e_2[j] = (rs2 == j);
+        end
+    endgenerate
+
+    generate
+        assign data_out_1[0] = {32{r_e_1[0]}} & gpr_reg[0];
+        assign data_out_2[0] = {32{r_e_2[0]}} & gpr_reg[0];
+        for (j = 1; j < 16; j = j + 1) begin
+            assign data_out_1[j] = data_out_1[j- 1] | ({32{r_e_1[j]}} & gpr_reg[j]);
+            assign data_out_2[j] = data_out_2[j- 1] | ({32{r_e_2[j]}} & gpr_reg[j]);
+        end
+    endgenerate
+
+    assign rf_busA = data_out_1[15];
+    assign rf_busB = data_out_2[15];
+
+endmodule
+
+
--- npc/multi_cpu/IDU/IDU_IMM_GEN_ysyx_23060136.sv
+++ npc/multi_cpu/IDU/IDU_IMM_GEN_ysyx_23060136.sv
@@ -1,38 +1,43 @@
 /*
  * @Author: Juqi Li @ NJU 
- * @Date: 2024-02-15 23:40:06 
+ * @Date: 2024-02-19 13:19:56 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-02-17 15:04:14
+ * @Last Modified time: 2024-02-19 20:25:07
  */
 
-`include "IDU_DEFINES_ysyx23060136.sv"
+ `include "IDU_DEFINES_ysyx23060136.sv"
 
-///////////////////////////////////////////////
+// ===========================================================================
 
 module IDU_IMM_GEN_ysyx_23060136(
-        input         [31 : 7]   inst,
-        input         [2 : 0]    opcode,   // 指令类型
-        output logic  [31 : 0]   imm
+        input   [31 : 0]   inst,
+        input              op_R_type,
+        input              op_I_type,
+        input              op_B_type,
+        input              op_J_type,
+        input              op_U_type,
+        input              op_S_type,
+        output  [31 : 0]   imm,
+        output             op_valid
     );
 
-    always_comb begin
-        unique case(opcode)
-            `I_type:                                                                          // I type
-                imm = {{20{inst[31]}}, inst[31 : 20]};
-            `S_type:                                                                          // S type
-                imm = {{20{inst[31]}}, inst[31 : 25], inst[11 : 7]};
-            `B_type:                                                                          // B type
-                imm = {{20{inst[31]}}, inst[7], inst[30 : 25], inst[11 : 8], 1'b0};
-            `U_type:                                                                          // U type
-                imm = {inst[31 : 12], 12'b0};
-            `J_type:                                                                          // J type
-                imm = {{12{inst[31]}}, inst[19 : 12], inst[20], inst[30 : 21], 1'b0};
-            default:
-                imm = 32'b0;                                                                  // R-type
-        endcase
-    end
+
+    assign imm =  ({32{op_I_type}} & {{20{inst[31]}}, inst[31 : 20]})                                |
+                  ({32{op_B_type}} & {{20{inst[31]}}, inst[7], inst[30 : 25], inst[11 : 8], 1'b0})   |
+                  ({32{op_S_type}} & {{20{inst[31]}}, inst[31 : 25], inst[11 : 7]})                  |
+                  ({32{op_U_type}} & {inst[31 : 12], 12'b0})                                         |
+                  ({32{op_J_type}} & {{12{inst[31]}}, inst[19 : 12], inst[20], inst[30 : 21], 1'b0}) | 
+                  ({32{op_R_type}} & 32'b0);
+
+    assign op_valid = (op_R_type ^ op_I_type ^ op_B_type ^ op_J_type ^ op_U_type ^ op_S_type) 
+                   & ~((op_R_type & op_I_type) | (op_R_type & op_B_type) | (op_R_type & op_J_type) 
+                    | (op_R_type & op_U_type) | (op_R_type & op_S_type) | (op_I_type & op_B_type) 
+                    | (op_I_type & op_J_type) | (op_I_type & op_U_type) | (op_I_type & op_S_type) 
+                    | (op_B_type & op_J_type) | (op_B_type & op_U_type) | (op_B_type & op_S_type) 
+                    | (op_J_type & op_U_type) | (op_J_type & op_S_type) | (op_U_type & op_S_type));
 
 endmodule
+
 
 
 
