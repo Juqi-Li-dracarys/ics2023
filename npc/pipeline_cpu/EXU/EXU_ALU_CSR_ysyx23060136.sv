386c7bec6a9b17776e9d01f6272c8b122b14f4f7
tracer-ysyx <tracer@ysyx.org>
2024-03-03 10:43:07 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:43:07 up  1:49,  1 user,  load average: 0.59, 0.66, 0.70

--- npc/pipeline_cpu/EXU/EXU_ALU_CSR_ysyx23060136.sv
+++ npc/pipeline_cpu/EXU/EXU_ALU_CSR_ysyx23060136.sv
@@ -11,13 +11,13 @@
  // module for dealing with csr 
 // ===========================================================================
 module EXU_ALU_CSR_ysyx23060136 (
-    input       [31 : 0]     EXU_pc,
-    input       [31 : 0]     EXU_HAZARD_rs1_data,
-    input       [31 : 0]     EXU_HAZARD_csr_rs_data,
-    input                    EXU_rv32_csrrs,
-    input                    EXU_rv32_csrrw,
-    input                    EXU_rv32_ecall,
-    output      [31 : 0]     EXU_ALU_CSR_out
+    input              [  31:0]         EXU_pc                     ,
+    input              [  31:0]         EXU_HAZARD_rs1_data        ,
+    input              [  31:0]         EXU_HAZARD_csr_rs_data     ,
+    input                               EXU_rv32_csrrs             ,
+    input                               EXU_rv32_csrrw             ,
+    input                               EXU_rv32_ecall             ,
+    output             [  31:0]         EXU_ALU_CSR_out             
  );
 
     assign EXU_ALU_CSR_out  = ({32{EXU_rv32_csrrs}}  & (EXU_HAZARD_rs1_data | EXU_HAZARD_csr_rs_data))  |
--- npc/pipeline_cpu/EXU/EXU_BRANCH_ysyx23060136.sv
+++ npc/pipeline_cpu/EXU/EXU_BRANCH_ysyx23060136.sv
@@ -11,31 +11,31 @@
 // ===========================================================================
 module EXU_BRANCH_ysyx23060136 (
         // data
-        input       [31 : 0]     EXU_pc,
-        input       [31 : 0]     EXU_HAZARD_rs1_data,
-        input       [31 : 0]     EXU_HAZARD_csr_rs_data,
-        input       [31 : 0]     EXU_imm,
-        input                    EXU_ALU_Less,
-        input                    EXU_ALU_Zero,
+        input              [  31:0]         EXU_pc                     ,
+        input              [  31:0]         EXU_HAZARD_rs1_data        ,
+        input              [  31:0]         EXU_HAZARD_csr_rs_data     ,
+        input              [  31:0]         EXU_imm                    ,
+        input                               EXU_ALU_Less               ,
+        input                               EXU_ALU_Zero               ,
 
         // jump/branch types isn't equal to jump signal
-        input                    EXU_jump,
-        input                    EXU_pc_plus_imm,
-        input                    EXU_rs1_plus_imm,
-        input                    EXU_csr_plus_imm,
+        input                               EXU_jump                   ,
+        input                               EXU_pc_plus_imm            ,
+        input                               EXU_rs1_plus_imm           ,
+        input                               EXU_csr_plus_imm           ,
         // signal is 0 means jump directly
-        input                    EXU_cmp_eq,
-        input                    EXU_cmp_neq,
-        input                    EXU_cmp_ge,
-        input                    EXU_cmp_lt,
+        input                               EXU_cmp_eq                 ,
+        input                               EXU_cmp_neq                ,
+        input                               EXU_cmp_ge                 ,
+        input                               EXU_cmp_lt                 ,
 
         // jump target
-        output      [31 : 0]     branch_target,
+        output             [  31:0]         branch_target              ,
         // jump signal
-        output                   PCSrc,
+        output                              PCSrc                      ,
         // 控制冒险
-        output                   BRANCH_flushIF,
-        output                   BRANCH_flushID
+        output                              BRANCH_flushIF             ,
+        output                              BRANCH_flushID              
     );
 
     wire   [31 : 0]  adder_da  = ({32{EXU_pc_plus_imm}}  & EXU_pc)                 |
