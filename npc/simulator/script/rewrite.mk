3368e1bed525f300c95be2db8514947fcd743134
tracer-ysyx <tracer@ysyx.org>
2024-03-03 12:08:49 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:08:49 up  3:15,  1 user,  load average: 0.58, 0.83, 0.90

--- npc/simulator/script/rewrite.mk
+++ npc/simulator/script/rewrite.mk
@@ -14,7 +14,7 @@ include VCPU_TOP_ysyx23060136.mk
 
 CXXFLAGS += -MMD -O3 -g -std=c++14 -fno-exceptions -fPIE -Wall
 CXXFLAGS += $(filter-out -D__STDC_FORMAT_MACROS, $(shell llvm-config-11 --cxxflags)) \
-			-fPIC -DDEVICE -D__GUEST_ISA__=$(ISA) -D__CPU_ARCH__=pipeline
+			-fPIC -DDEVICE -D__GUEST_ISA__=$(ISA) -D__CPU_ARCH__=$(CPU_ARCH)
 LDFLAGS += -O3 -rdynamic -shared -fPIC
 LIBS += $(shell llvm-config-11 --libs)
 LIBS += -lreadline -ldl -pie -lSDL2
