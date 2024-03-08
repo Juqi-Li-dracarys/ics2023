3737d6c2f7060dbd10ba49a405ead6e4785c4460
tracer-ics2023 <tracer@njuics.org>
2024-03-08 17:41:10 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:41:10 up  8:35,  1 user,  load average: 0.51, 0.48, 0.51

--- npc/tests/char-test.c
+++ npc/tests/char-test.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-03-08 08:52:48 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-08 10:00:01
+ * @Last Modified time: 2024-03-08 17:41:02
  */
 
 
@@ -12,6 +12,5 @@
 
 void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'A' ;
-  *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
 }
