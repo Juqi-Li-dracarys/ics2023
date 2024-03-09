14563eec3f10d29b0f01316874b47bff1077b0e2
tracer-ysyx <tracer@ysyx.org>
2024-03-09 20:50:22 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:22 up  1:33,  1 user,  load average: 1.01, 1.23, 1.00

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -40,8 +40,7 @@ void halt(int code) {
 void _trm_init() {
   // boot loader
   // printf("data_start: %p load_start: %p\n", &_data_start,  &_data_load_start);
-  uint32_t start = (uint32_t)(&_data_start);
-  putch((char)(start));
+  putch('a');
   if (&_data_start != &_data_load_start) {
     memcpy(&_data_start, &_data_load_start, (size_t)&_data_size);
   }
