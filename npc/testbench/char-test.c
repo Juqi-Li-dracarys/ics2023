a97601ac0365d2c7a4d22c092ac5b3e1696c5d1d
tracer-ics2023 <tracer@njuics.org>
2024-03-09 09:27:39 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:27:39 up  1:12,  1 user,  load average: 1.48, 1.03, 0.75

--- npc/testbench/char-test.c
+++ npc/testbench/char-test.c
@@ -10,10 +10,10 @@
 #define UART_BASE  0x10000000
 #define UART_TX    0
 
-// char x = 104;
+char x = 104;
 
 void _start() {
-  *(volatile char *)(UART_BASE + UART_TX) =  'h' ;
+  *(volatile char *)(UART_BASE + UART_TX) = x ;
   *(volatile char *)(UART_BASE + UART_TX) = 'e' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
@@ -23,6 +23,6 @@ void _start() {
   *(volatile char *)(UART_BASE + UART_TX) = 'o' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'C' ;
   *(volatile char *)(UART_BASE + UART_TX) = '!' ;
-  *(volatile char *)(UART_BASE + UART_TX) = '\n' ;
+  *(volatile char *)(UART_BASE + UART_TX) = '\n';
   while (1);
 }
