44f492624c8f001af7a85cb5c7f60d93657a4b22
tracer-ysyx <tracer@ysyx.org>
2024-03-09 09:26:23 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:26:23 up  1:11,  1 user,  load average: 1.09, 0.89, 0.69

--- npc/testbench/char-test.c
+++ npc/testbench/char-test.c
@@ -10,10 +10,10 @@
 #define UART_BASE  0x10000000
 #define UART_TX    0
 
-char x = 104;
+// char x = 104;
 
 void _start() {
-  *(volatile char *)(UART_BASE + UART_TX) =  x ;
+  *(volatile char *)(UART_BASE + UART_TX) =  'h' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'e' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
