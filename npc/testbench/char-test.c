a948721a96777d08afa4f109bc541e26051a1425
tracer-ysyx <tracer@ysyx.org>
2024-03-09 09:24:19 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:24:19 up  1:09,  1 user,  load average: 1.07, 0.86, 0.65

--- npc/testbench/char-test.c
+++ npc/testbench/char-test.c
@@ -10,19 +10,19 @@
 #define UART_BASE  0x10000000
 #define UART_TX    0
 
-int x = 50;
+char x = 104;
 
 void _start() {
-  *(volatile char *)(UART_BASE + UART_TX) = 'H' ;
-  *(volatile char *)(UART_BASE + UART_TX) = 'E' ;
-  *(volatile char *)(UART_BASE + UART_TX) = 'L' ;
-  *(volatile char *)(UART_BASE + UART_TX) = 'L' ;
-  *(volatile char *)(UART_BASE + UART_TX) = 'O' ;
+  *(volatile char *)(UART_BASE + UART_TX) =  x ;
+  *(volatile char *)(UART_BASE + UART_TX) = 'e' ;
+  *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
+  *(volatile char *)(UART_BASE + UART_TX) = 'l' ;
+  *(volatile char *)(UART_BASE + UART_TX) = 'o' ;
   *(volatile char *)(UART_BASE + UART_TX) = ',' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'S' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'o' ;
   *(volatile char *)(UART_BASE + UART_TX) = 'C' ;
-  *(volatile char *)(UART_BASE + UART_TX) = x ;
+  *(volatile char *)(UART_BASE + UART_TX) = '!' ;
   *(volatile char *)(UART_BASE + UART_TX) = '\n' ;
   while (1);
 }
