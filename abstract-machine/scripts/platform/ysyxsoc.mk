ff2d378a4e2a3e6f8b0ceb8192859409bcc00266
tracer-ics2023 <tracer@njuics.org>
2024-03-12 19:55:36 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:55:36 up 25 min,  1 user,  load average: 0.55, 0.34, 0.33

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -56,7 +56,7 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 # @$(OBJCOPY) -S -O binary $(IMAGE).elf $(IMAGE).bin
-	@$(OBJCOPY) -S --set-section-flags .bss=contents -O binary $(IMAGE).elf $(IMAGE).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc -O binary $(IMAGE).elf $(IMAGE).bin
 
 # load the image on ysyxsoc simulator
 
