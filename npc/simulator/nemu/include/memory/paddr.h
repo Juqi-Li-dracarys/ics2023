c93cc48db8627ec3a79e1f9c2cebe99b00101c8b
tracer-ics2023 <tracer@njuics.org>
2024-03-15 23:06:03 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:06:03 up  2:38,  1 user,  load average: 1.78, 1.59, 1.08

--- npc/simulator/nemu/include/memory/paddr.h
+++ npc/simulator/nemu/include/memory/paddr.h
@@ -18,15 +18,15 @@
 
 #include <common.h>
 
-#define RESET_VECTOR (CONFIG_MROM_MBASE + CONFIG_PC_RESET_OFFSET)
+#define RESET_VECTOR (CONFIG_FLASH_MBASE + CONFIG_PC_RESET_OFFSET)
 
 /* convert the guest physical address in the guest program to host virtual address in NEMU */
 uint8_t* guest_to_host(paddr_t paddr);
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
 paddr_t host_to_guest(uint8_t *haddr);
 
-static inline bool in_mrom(paddr_t addr) {
-  return addr - CONFIG_MROM_MBASE < CONFIG_MROM_SIZE;
+static inline bool in_flash(paddr_t addr) {
+  return addr - CONFIG_FLASH_MBASE < CONFIG_FLASH_SIZE;
 }
 
 static inline bool in_sram(paddr_t addr) {
