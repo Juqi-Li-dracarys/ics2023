2c7909f523f8b2abad9681bfc20da3a73977e088
tracer-ysyx <tracer@ysyx.org>
2024-03-16 10:41:38 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:38 up 32 min,  1 user,  load average: 1.03, 0.77, 0.59

---
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/timer.c
@@ -0,0 +1,29 @@
+/*
+ * @Author: Juqi Li @ NJU 
+ * @Date: 2024-03-16 10:33:54 
+ * @Last Modified by: Juqi Li @ NJU
+ * @Last Modified time: 2024-03-16 10:41:17
+ */
+
+
+#include <am.h>
+#include <ysyxsoc.h>
+
+void __am_timer_init() {
+    
+}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+//   uint64_t us_lower = (uint64_t)inl(RTC_ADDR);
+//   uint64_t us_higher = (uint64_t)inl(RTC_ADDR + 4);
+//   uptime->us = us_lower | (us_higher << 32);
+}
+
+void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
+  rtc->second = 0;
+  rtc->minute = 0;
+  rtc->hour   = 0;
+  rtc->day    = 0;
+  rtc->month  = 0;
+  rtc->year   = 1900;
+}
