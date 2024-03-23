b6171f04dd92c21c9ecc9c160e621cec8e0b8a92
tracer-ysyx <tracer@ysyx.org>
2024-03-23 16:36:21 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:21 up  5:28,  1 user,  load average: 1.31, 1.61, 1.46

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-18 20:54:49 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-23 16:34:54
+ * @Last Modified time: 2024-03-23 16:36:09
  */
 
 #include <am.h>
@@ -74,7 +74,7 @@ static void chip_info() {
     for(i = 0; i < 4; i++) {
         *(volatile uint16_t *)(LED_BASE) = 0x0;
         j = 1000000;
-        while (j-- > 0);
+        while (1);
         *(volatile uint16_t *)(LED_BASE) = 0xFFFF;
         j = 1000000;
         while (j-- > 0);
