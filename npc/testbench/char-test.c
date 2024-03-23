4d4a6ef2e1ae7e445ecf068f23029a6359da1631
tracer-ics2023 <tracer@njuics.org>
2024-03-23 23:22:03 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:22:03 up  2:52,  1 user,  load average: 0.68, 0.49, 0.55

--- npc/testbench/char-test.c
+++ npc/testbench/char-test.c
@@ -2,22 +2,51 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-03-08 08:52:48 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-23 17:33:12
+ * @Last Modified time: 2024-03-23 18:16:27
  */
 
+#define UART_BASE    0x10000000
+#define UART_TX      0
+#define UART_DIV_L   0
+#define UART_DIV_M   1
+#define UART_FCR     2
+// line control register
+#define UART_LCR     3
+#define UART_MCR     4
+#define UART_LSR     5
 
-// 输出第一个字符
-#define UART_BASE  0x10000000
+
 #define FLASH_BASE 0x30000000
-#define UART_TX    0
-
 #define SDRAM_BASE 0xa0000000
 #define TEST_START 0x0000FFF0
-// #define TEST_START 0x0
 #define TEST_END   0x00010000
+
 
 void _start() {
 
+    // init uart 目前只支持 8N1 的串口传输配置
+
+    // • Set the Line Control Register to the desired line control parameters. Set bit 7 to ‘1’ 
+    // to allow access to the Divisor Latches.(1000 0011)
+    *(volatile char *)(UART_BASE + UART_LCR) =  0x83;
+
+    // • Set the Divisor Latches, MSB first, LSB next.
+    *(volatile char *)(UART_BASE + UART_DIV_M) =  0x83;
+    *(volatile char *)(UART_BASE + UART_DIV_L) =  0x83;
+
+    // • Set bit 7 of LCR to ‘0’ to disable access to Divisor Latches. At this time the 
+    // transmission engine starts working and data can be sent and received. 
+
+
+    // • Set the FIFO trigger level. Generally, higher trigger level values produce less 
+    // interrupt to the system, so setting it to 14 bytes is recommended if the system 
+    // responds fast enough. 
+
+
+    // • Enable desired interrupts by setting appropriate bits in the Interrupt Enable 
+    // register. 
+
+    
     *(volatile char *)(UART_BASE) = 'h' ;
     *(volatile char *)(UART_BASE) = 'e' ;
     *(volatile char *)(UART_BASE) = 'l' ;
@@ -39,7 +68,7 @@ void _start() {
     *(volatile char *)(UART_BASE) = 'S' ;
     *(volatile char *)(UART_BASE) = 'o' ;
     *(volatile char *)(UART_BASE) = 'C' ;
-    // *(volatile char *)(UART_BASE) = '!' ;
+    *(volatile char *)(UART_BASE) = '!' ;
     *(volatile char *)(UART_BASE) = '\n';
 
     asm volatile(
