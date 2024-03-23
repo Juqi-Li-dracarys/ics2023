da8dae4257ddd3b7533c0b35a092db3d43bf079f
tracer-ics2023 <tracer@njuics.org>
2024-03-23 17:32:09 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:32:09 up  6:24,  1 user,  load average: 2.04, 1.59, 1.10

--- npc/testbench/char-test.c
+++ npc/testbench/char-test.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-03-08 08:52:48 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-23 17:31:04
+ * @Last Modified time: 2024-03-23 17:31:50
  */
 
 
@@ -18,29 +18,31 @@
 
 void _start() {
 
-  *(volatile char *)(UART_BASE) = 'h' ;
-  *(volatile char *)(UART_BASE) = 'e' ;
-  *(volatile char *)(UART_BASE) = 'l' ;
-  *(volatile char *)(UART_BASE) = 'l' ;
-  *(volatile char *)(UART_BASE) = 'o' ;
-  *(volatile char *)(UART_BASE) = ',' ;
-  *(volatile char *)(UART_BASE) = 'S' ;
-  *(volatile char *)(UART_BASE) = 'o' ;
-  *(volatile char *)(UART_BASE) = 'C' ;
-  *(volatile char *)(UART_BASE) = '!' ;
-  *(volatile char *)(UART_BASE) = '\n';
+    *(volatile char *)(UART_BASE) = 'h' ;
+    *(volatile char *)(UART_BASE) = 'e' ;
+    *(volatile char *)(UART_BASE) = 'l' ;
+    *(volatile char *)(UART_BASE) = 'l' ;
+    *(volatile char *)(UART_BASE) = 'o' ;
+    *(volatile char *)(UART_BASE) = ',' ;
+    *(volatile char *)(UART_BASE) = 'S' ;
+    *(volatile char *)(UART_BASE) = 'o' ;
+    *(volatile char *)(UART_BASE) = 'C' ;
+    *(volatile char *)(UART_BASE) = '!' ;
+    *(volatile char *)(UART_BASE) = '\n';
 
-  *(volatile char *)(UART_BASE) = 'h' ;
-  *(volatile char *)(UART_BASE) = 'e' ;
-  *(volatile char *)(UART_BASE) = 'l' ;
-   *(volatile char *)(UART_BASE) = 'l' ;
+    *(volatile char *)(UART_BASE) = 'h' ;
+    *(volatile char *)(UART_BASE) = 'e' ;
+    *(volatile char *)(UART_BASE) = 'l' ;
+    *(volatile char *)(UART_BASE) = 'l' ;
+    *(volatile char *)(UART_BASE) = 'o' ;
+    *(volatile char *)(UART_BASE) = '\n';
 
-  asm volatile(
-    "mv a0, %0\n\t"
-    "ebreak"
-    : 
-    :"r"(0)
-  );
-  // shoud not reach here
-  while (1);
+    asm volatile(
+        "mv a0, %0\n\t"
+        "ebreak"
+        : 
+        :"r"(0)
+    );
+    // shoud not reach here
+    while (1);
 }
