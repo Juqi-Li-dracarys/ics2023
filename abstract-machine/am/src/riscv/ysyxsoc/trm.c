8bc97a2578d81120caf75fd68a1aea2d86921248
tracer-ics2023 <tracer@njuics.org>
2024-03-23 16:30:14 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:14 up  5:22,  1 user,  load average: 2.23, 1.96, 1.46

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-18 20:54:49 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-23 16:24:11
+ * @Last Modified time: 2024-03-23 16:30:10
  */
 
 #include <am.h>
@@ -58,6 +58,7 @@ uint32_t value_hex(uint32_t a) {
 static void chip_info() {
     volatile uint32_t value;
     uint32_t hex_value;
+    uint32_t j;
     asm volatile ("csrr %0, mvendorid" : "=r" (value));
     printf("CREATOR: %c%c%c%c", (char)(value >> 24), (char)(value >> 16), (char)(value >> 8), (char)(value));
     asm volatile ("csrr %0, marchid" : "=r" (value));
@@ -67,6 +68,13 @@ static void chip_info() {
     for(int i = 0; i < 4; i++) {
         *(volatile char *)(SEG_BASE + i) = hex_value & 0xFF;
         hex_value = hex_value >> 8;
+    }
+    // LED twinkle
+    for(int i = 0; i < 4; i++) {
+        *(volatile uint16_t *)(LED_BASE) = 0x0;
+        do { j = 100;} while (j-- > 0);
+        *(volatile uint16_t *)(LED_BASE) = 0xFF;
+        do { j = 100;} while (j-- > 0);
     }
     return;
 }
