34450707d590aa53c24f01a2667d3f8d24682cd0
tracer-ics2023 <tracer@njuics.org>
2024-03-24 11:16:43 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:43 up  1:19,  1 user,  load average: 1.38, 1.06, 0.92

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-03-16 10:34:01 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-16 11:04:45
+ * @Last Modified time: 2024-03-24 11:16:36
  */
 
 #include <am.h>
@@ -11,16 +11,21 @@
 void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
-
+void __am_uart_rx(AM_UART_RX_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
+static void __am_uart_config (AM_UART_CONFIG_T*cfg)   { cfg->present = true;  }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false; }
 
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
   [AM_TIMER_RTC   ] = __am_timer_rtc,
-  [AM_TIMER_UPTIME] = __am_timer_uptime
+  [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_UART_CONFIG]  = __am_uart_config,
+  [AM_UART_RX]      = __am_uart_rx,
+  [AM_INPUT_CONFIG] = __am_input_config
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
---
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/uart.c
@@ -0,0 +1,21 @@
+/*
+ * @Author: Juqi Li @ NJU 
+ * @Date: 2024-03-24 11:10:30 
+ * @Last Modified by: Juqi Li @ NJU
+ * @Last Modified time: 2024-03-24 11:16:31
+ */
+
+#include <am.h>
+#include <ysyxsoc.h>
+
+void __am_uart_rx(AM_UART_RX_T *uart_rx) {
+    if(*(volatile char *)(UART_BASE + UART_LSR) & 0x01) {
+        uart_rx->data =  *(volatile char *)(UART_BASE + UART_RX);
+    }
+    else {
+        uart_rx->data = 0xff;
+    }
+    return;
+}
+
+
