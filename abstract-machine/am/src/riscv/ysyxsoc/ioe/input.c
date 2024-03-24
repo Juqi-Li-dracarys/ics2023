8cf62ba6347829fb94946093f709aefec01c7b79
tracer-ysyx <tracer@ysyx.org>
2024-03-24 16:03:59 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:03:59 up  2:45,  1 user,  load average: 1.00, 0.65, 0.53

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/input.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-03-24 14:36:47 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-03-24 15:52:55
+ * @Last Modified time: 2024-03-24 16:03:51
  */
 
 
@@ -14,9 +14,14 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
     uint8_t data = *(volatile char *)(PS2_BASE);
-    if(data != 0) {
-        printf("%p\n", data);
+    if(data != 0xf0) {
+        printf("down: %p\n", data);
     }
+    else {
+        data = *(volatile char *)(PS2_BASE);
+        printf("up: %p\n", data);
+    }
+    
     kbd->keycode = 0;
     kbd->keydown = 0;
     return;
