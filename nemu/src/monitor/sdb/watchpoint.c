9695314905072a7c4c2cf8d79fbc5d46bf8cd19c
tracer-ics2023 <tracer@njuics.org>
2024-04-02 15:29:40 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:40 up  4:56,  1 user,  load average: 1.28, 1.75, 1.66

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -2,7 +2,7 @@
  * @Author: Juqi Li @ NJU 
  * @Date: 2024-01-11 23:44:45 
  * @Last Modified by: Juqi Li @ NJU
- * @Last Modified time: 2024-04-02 15:27:29
+ * @Last Modified time: 2024-04-02 15:29:30
  */
 
 // source code for wp bp
@@ -144,3 +144,4 @@ void delete_bp(void) {
   printf("Delete the break point @PC = 0x%016lx\n", pc_addr);
   return;
 }
+
