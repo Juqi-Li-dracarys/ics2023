087e6c04a5b4e2ce0c8dc7ec3e5647d1003ece33
tracer-ysyx <tracer@ysyx.org>
2024-04-12 20:57:53 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:53 up  2:14,  1 user,  load average: 1.50, 0.82, 0.58

--- npc/simulator/sim/device/timer.cpp
+++ npc/simulator/sim/device/timer.cpp
@@ -9,15 +9,9 @@ static void rtc_io_handler(paddr_t offset, int len, bool is_write) {
   struct tm* rtc = get_time_tm();
   rtc_port_base[0] = (uint32_t)us;
   rtc_port_base[1] = us >> 32;
-  rtc_port_base[2] = rtc->tm_sec;
-  rtc_port_base[3] = rtc->tm_min;
-  rtc_port_base[4] = rtc->tm_hour;
-  rtc_port_base[5] = rtc->tm_mday;
-  rtc_port_base[6] = rtc->tm_mon + 1;
-  rtc_port_base[7] = rtc->tm_year + 1900;
 }
 
 void init_timer() {
-  rtc_port_base = (uint32_t *)new_space(32);
-  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 32, rtc_io_handler);
+  rtc_port_base = (uint32_t *)new_space(8);
+  add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
 }
