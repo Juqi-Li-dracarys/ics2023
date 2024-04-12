cf724acd16603a83042ed66e87c23316316c8ac3
tracer-ics2023 <tracer@njuics.org>
2024-04-12 17:00:48 +0800

    > compile NEMU 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:00:48 up  3:43,  1 user,  load average: 0.89, 0.64, 0.54

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -138,13 +138,13 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
     // if nemu get ebreak or abortion, this log will be displyed
     case NEMU_END: case NEMU_ABORT:
-#ifndef CONFIG_TARGET_SHARE
+
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-#endif
+
     // fall through
     case NEMU_QUIT: statistic();
   }
