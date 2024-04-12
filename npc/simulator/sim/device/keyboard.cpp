3a7443195d5b22a0af322da15b5e2dd81be9de33
tracer-ysyx <tracer@ysyx.org>
2024-04-12 12:01:07 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:07 up  2:57,  1 user,  load average: 2.55, 2.64, 1.94

--- npc/simulator/sim/device/keyboard.cpp
+++ npc/simulator/sim/device/keyboard.cpp
@@ -56,7 +56,7 @@ void send_key(uint8_t scancode, bool is_keydown) {
   }
 }
 
-static uint8_t *i8042_data_port_base = NULL;
+static uint32_t *i8042_data_port_base = NULL;
 
 static void i8042_data_io_handler(paddr_t offset, int len, bool is_write) {
   assert(!is_write);
@@ -65,7 +65,7 @@ static void i8042_data_io_handler(paddr_t offset, int len, bool is_write) {
 }
 
 void init_i8042() {
-  i8042_data_port_base = (uint8_t *)new_space(4);
+  i8042_data_port_base = (uint32_t *)new_space(4);
   i8042_data_port_base[0] = _KEY_NONE;
   add_mmio_map("keyboard", CONFIG_I8042_DATA_MMIO, i8042_data_port_base, 4, i8042_data_io_handler);
   init_keymap();
