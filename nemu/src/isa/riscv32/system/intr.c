2fd9b3db73bad378958914abd868c0491fd17bb5
tracer-ysyx <tracer@ysyx.org>
2024-04-13 21:16:20 +0800

    > compile NEMU ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:20 up  1:31,  1 user,  load average: 0.73, 1.08, 0.78

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,15 +18,15 @@
 // 关闭中断使能
 // 将 mstatus.MIE 保存到 mstatus.MPIE 中, 然后将 mstatus.MIE 位置为 0
 void reset_intr() {
-  cpu.csr[_mstatus] = (cpu.csr[_mstatus] & ~(0x1 << MPIE_OFFSET)) | (((cpu.csr[_mstatus] >> MIE_OFFSET) & 0x1) << MPIE_OFFSET) ;
-  cpu.csr[_mstatus] &= ~(1 << MIE_OFFSET);
+//   cpu.csr[_mstatus] = (cpu.csr[_mstatus] & ~(0x1 << MPIE_OFFSET)) | (((cpu.csr[_mstatus] >> MIE_OFFSET) & 0x1) << MPIE_OFFSET) ;
+//   cpu.csr[_mstatus] &= ~(1 << MIE_OFFSET);
 }
 
 // 开启中断使能
 // 将 mstatus.MPIE 还原到 mstatus.MIE 中, 然后将 mstatus.MPIE 位置为 1
 void set_intr() { 
-  cpu.csr[_mstatus] = (cpu.csr[_mstatus] & ~(0x1 << MIE_OFFSET)) | (((cpu.csr[_mstatus] >> MPIE_OFFSET) & 0x1) << MIE_OFFSET);
-  cpu.csr[_mstatus] |= (1 << MPIE_OFFSET);
+//   cpu.csr[_mstatus] = (cpu.csr[_mstatus] & ~(0x1 << MIE_OFFSET)) | (((cpu.csr[_mstatus] >> MPIE_OFFSET) & 0x1) << MIE_OFFSET);
+//   cpu.csr[_mstatus] |= (1 << MPIE_OFFSET);
 }
 
 
