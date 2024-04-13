58e116e2be27b0a9e4b86c4dacb83982a660b512
tracer-ysyx <tracer@ysyx.org>
2024-04-13 17:51:04 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:51:04 up 52 min,  1 user,  load average: 0.70, 0.51, 0.45

--- npc/core/EXU/ysyx_23060136_EXU_SHIFT.sv
+++ npc/core/EXU/ysyx_23060136_EXU_SHIFT.sv
@@ -19,6 +19,7 @@ module ysyx_23060136_EXU_SHIFT #(
     (
         input        [data_width-1 : 0]    din   ,     // The data designed to be shifted
         input        [shamt_width-1 : 0]   shamt ,     // The bit of this shifting
+        input                              word_t,
         input                              LR    ,     // when LR=1, left_shift, else right_shift
         input                              AL    ,     // when AL=1, algorithm_shift, else logic_shift
         output logic [data_width-1 : 0]    dout        // Output data
@@ -27,15 +28,31 @@ module ysyx_23060136_EXU_SHIFT #(
     // One interpretation of algorithm shift
     always_comb begin
         // logic shift
-        if(AL == 1'b0 || (AL == 1'b1 && LR == 1'b1))
-            dout = LR ? din << shamt : din >> shamt;
+        if(word_t) begin
+            if(AL == 1'b0 || (AL == 1'b1 && LR == 1'b1))
+                dout = {32'b0, {LR ? din[31 : 0] << shamt[4 : 0] : din[31 : 0] >> shamt[4 : 0]}};
         // algorithm_shift(right)
+            else begin
+                dout[31 : 0] = shamt[0] ? {din[31]        , din[31 : 1]}         :  din[31 : 0];
+                dout[31 : 0] = shamt[1] ? {{2{dout[32-1]}},  dout[32-1 : 2]}     :  dout[31 : 0];
+                dout[31 : 0] = shamt[2] ? {{4{dout[32-1]}},  dout[32-1 : 4]}     :  dout[31 : 0];
+                dout[31 : 0] = shamt[3] ? {{8{dout[32-1]}},  dout[32-1 : 8]}     :  dout[31 : 0];
+                dout[31 : 0] = shamt[4] ? {{16{dout[32-1]}}, dout[32-1 : 16]}    :  dout[31 : 0];
+                dout[data_width-1 : 32] = `ysyx_23060136_false;
+            end
+        end
         else begin
-            dout = shamt[0] ? {din[data_width-1],        din[data_width-1 : 1]} :   din;
-            dout = shamt[1] ? {{2{dout[data_width-1]}},  dout[data_width-1 : 2]} :  dout;
-            dout = shamt[2] ? {{4{dout[data_width-1]}},  dout[data_width-1 : 4]} :  dout;
-            dout = shamt[3] ? {{8{dout[data_width-1]}},  dout[data_width-1 : 8]} :  dout;
-            dout = shamt[4] ? {{16{dout[data_width-1]}}, dout[data_width-1 : 16]} : dout;
+            if(AL == 1'b0 || (AL == 1'b1 && LR == 1'b1))
+                dout = LR ? din << shamt : din >> shamt;
+        // algorithm_shift(right)
+            else begin
+                dout = shamt[0] ? {din[data_width-1],        din[data_width-1 : 1]} :   din;
+                dout = shamt[1] ? {{2{dout[data_width-1]}},  dout[data_width-1 : 2]} :  dout;
+                dout = shamt[2] ? {{4{dout[data_width-1]}},  dout[data_width-1 : 4]} :  dout;
+                dout = shamt[3] ? {{8{dout[data_width-1]}},  dout[data_width-1 : 8]} :  dout;
+                dout = shamt[4] ? {{16{dout[data_width-1]}}, dout[data_width-1 : 16]} : dout;
+                dout = shamt[5] ? {{32{dout[data_width-1]}}, dout[data_width-1 : 32]} : dout;
+            end
         end
     end
 
