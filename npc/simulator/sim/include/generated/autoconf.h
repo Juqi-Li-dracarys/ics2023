5edb835907e10d5cf42c1bb5ef4b1255698855c7
tracer-ics2023 <tracer@njuics.org>
2024-04-23 17:46:20 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:46:20 up  1:13,  1 user,  load average: 1.07, 0.93, 0.79

--- npc/simulator/sim/include/generated/autoconf.h
+++ npc/simulator/sim/include/generated/autoconf.h
@@ -48,26 +48,26 @@
 #define CONFIG_PMEM_GARRAY 1
 #define CONFIG_FFB_ADDR 0xa2000000
 
-// // Trace
-// #define CONFIG_TRACE 1
+// Trace
+#define CONFIG_TRACE 1
 
-// // Itrace
-// #define CONFIG_ITRACE 1
-// #define CONFIG_ITRACE_COND 1
-// #define ITRACE_COND  1
+// Itrace
+#define CONFIG_ITRACE 1
+#define CONFIG_ITRACE_COND 1
+#define ITRACE_COND  1
 
-// // Itrace max record number setting
-// #define CONFIG_TRACE_START 0
-// #define CONFIG_TRACE_END   100000
+// Itrace max record number setting
+#define CONFIG_TRACE_START 0
+#define CONFIG_TRACE_END   100000
 
-// // diff test
-// #define CONFIG_DIFFTEST 1
+// diff test
+#define CONFIG_DIFFTEST 1
 
-// // mem trace
-// #define CONFIG_MTRACE_COND 1
+// mem trace
+#define CONFIG_MTRACE_COND 1
 
-// // watch/break point
-// #define CONFIG_WBCHECK 1
+// watch/break point
+#define CONFIG_WBCHECK 1
 
 // // wave record
 // #define WAVE_RECORD 1
