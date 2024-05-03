468d1c0052d71a484ad9e2f38d97bb6229a9da8d
tracer-ysyx <tracer@ysyx.org>
2024-05-03 15:28:30 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:30 up  2:12,  1 user,  load average: 1.24, 1.24, 1.18

--- npc/core/EXU/ysyx_23060136_EXU_DIV.sv
+++ npc/core/EXU/ysyx_23060136_EXU_DIV.sv
@@ -19,10 +19,10 @@ module ysyx_23060136_EXU_DIV(
         input                                                       div_valid      ,      
         input                                                       divw	       ,      
         input                                                       div_signed     ,   
-        output  logic                                               div_ready      ,  
-        output  logic                                               div_out_valid  ,   
-        output  logic  [  `ysyx_23060136_BITS_W-1:0 ]               quotient       , 
-        output  logic  [  `ysyx_23060136_BITS_W-1:0 ]               remainder  
+        output                                                      div_ready      ,  
+        output                                                      div_out_valid  ,   
+        output        [  `ysyx_23060136_BITS_W-1:0 ]                quotient       , 
+        output        [  `ysyx_23060136_BITS_W-1:0 ]                remainder  
  );
 
 
@@ -37,29 +37,32 @@ module ysyx_23060136_EXU_DIV(
 
 // ===========================================================================
 // function  interface
- wire            [`ysyx_23060136_BITS_W-1 : 0]     DIV_dword_u   =  $unsigned(dividend) / $unsigned(divisor);
- wire            [`ysyx_23060136_BITS_W-1 : 0]     DIV_dword_s   =  $signed(dividend)   / $signed(divisor);
+ wire         [`ysyx_23060136_BITS_W-1 : 0]     DIV_dword_u   =  $unsigned(dividend) / $unsigned(divisor);
+ wire         [`ysyx_23060136_BITS_W-1 : 0]     DIV_dword_s   =  $signed(dividend)   / $signed(divisor);
 
- wire            [31 : 0]                          DIV_word_u    =  $unsigned(dividend[31 : 0]) / $unsigned(divisor[31 : 0]);    
- wire            [31 : 0]                          DIV_word_s    =  $signed(dividend[31 : 0])   / $signed(divisor[31 : 0]);
+ wire         [31 : 0]                          DIV_word_u    =  $unsigned(dividend[31 : 0]) / $unsigned(divisor[31 : 0]);    
+ wire         [31 : 0]                          DIV_word_s    =  $signed(dividend[31 : 0])   / $signed(divisor[31 : 0]);
 
- wire            [`ysyx_23060136_BITS_W-1 : 0]     REM_dword_u   =  $unsigned(dividend) % $unsigned(divisor);
- wire            [`ysyx_23060136_BITS_W-1 : 0]     REM_dword_s   =  $signed(dividend)   % $signed(divisor);
+ wire         [`ysyx_23060136_BITS_W-1 : 0]     REM_dword_u   =  $unsigned(dividend) % $unsigned(divisor);
+ wire         [`ysyx_23060136_BITS_W-1 : 0]     REM_dword_s   =  $signed(dividend)   % $signed(divisor);
 
- wire            [31 : 0]                          REM_word_u    =  $unsigned(dividend[31 : 0]) % $unsigned(divisor[31 : 0]);    
- wire            [31 : 0]                          REM_word_s    =  $signed(dividend[31 : 0])   % $signed(divisor[31 : 0]);
+ wire         [31 : 0]                          REM_word_u    =  $unsigned(dividend[31 : 0]) % $unsigned(divisor[31 : 0]);    
+ wire         [31 : 0]                          REM_word_s    =  $signed(dividend[31 : 0])   % $signed(divisor[31 : 0]);
  
  
- wire            [  `ysyx_23060136_BITS_W-1:0 ] MUL_result_quotient  =  {64{!divw & !div_signed}}  &  DIV_dword_u   | 
-                                                                        {64{!divw &  div_signed}}  &  DIV_dword_s   |
-                                                                        {64{divw  &  !div_signed}} &  {{32{DIV_word_u[31]}}, DIV_word_u} |
-                                                                        {64{divw  &  div_signed}}  &  {{32{DIV_word_s[31]}}, DIV_word_s};
+ assign                                         quotient      =    {64{!divw & !div_signed}}  &  DIV_dword_u   | 
+                                                                   {64{!divw &  div_signed}}  &  DIV_dword_s   |
+                                                                   {64{divw  &  !div_signed}} &  {{32{DIV_word_u[31]}}, DIV_word_u} |
+                                                                   {64{divw  &  div_signed}}  &  {{32{DIV_word_s[31]}}, DIV_word_s};
 
-
- wire            [  `ysyx_23060136_BITS_W-1:0 ] MUL_result_remainder =  {64{!divw & !div_signed}}  &  REM_dword_u   | 
-                                                                        {64{!divw &  div_signed}}  &  REM_dword_s   |
-                                                                        {64{divw  &  !div_signed}} &  {{32{REM_word_u[31]}}, REM_word_u} |
-                                                                        {64{divw  &  div_signed}}  &  {{32{REM_word_s[31]}}, REM_word_s};                                                            
+ assign                                         remainder     =    {64{!divw & !div_signed}}  &  REM_dword_u   | 
+                                                                   {64{!divw &  div_signed}}  &  REM_dword_s   |
+                                                                   {64{divw  &  !div_signed}} &  {{32{REM_word_u[31]}}, REM_word_u} |
+                                                                   {64{divw  &  div_signed}}  &  {{32{REM_word_s[31]}}, REM_word_s};   
+  
+ assign                                         div_ready     =    state_idle                                ;
+ assign                                         div_out_valid =    &cyc_counter                              ;
+                                                                    
 // ===========================================================================
 
  always_comb begin : next_state_update
@@ -73,7 +76,7 @@ module ysyx_23060136_EXU_DIV(
          end
      end
      `ysyx_23060136_ready: begin
-         if(cyc_counter == 2'd3) begin
+         if(&cyc_counter) begin
              next_state = `ysyx_23060136_idle;
          end
          else begin
@@ -99,32 +102,6 @@ module ysyx_23060136_EXU_DIV(
      end
      else if(state_ready)begin
          cyc_counter <= cyc_counter + 1;
-     end
- end
-
- always_ff @(posedge clk) begin : div_ready_update
-     if(rst) begin
-         div_ready     <= `ysyx_23060136_true;
-         div_out_valid <= `ysyx_23060136_false;
-     end
-     else if((state_idle & next_state == `ysyx_23060136_ready)) begin
-         div_ready     <= `ysyx_23060136_false;
-         div_out_valid <= `ysyx_23060136_false;
-     end
-     else if(cyc_counter == 2'd3) begin
-         div_ready     <= `ysyx_23060136_true;
-         div_out_valid <= `ysyx_23060136_true;
-     end
- end
-
- always_ff @(posedge clk) begin : div_result_update
-     if(rst) begin
-        quotient   <= `ysyx_23060136_false;
-        remainder  <= `ysyx_23060136_false;
-     end
-     else if(cyc_counter == 2'd3)begin
-        quotient   <=  MUL_result_quotient;
-        remainder  <=  MUL_result_remainder;
      end
  end
 
