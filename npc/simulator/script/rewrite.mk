60599d90018639549775db5d4500a0ed58712f04
tracer-ysyx <tracer@ysyx.org>
2024-05-04 11:07:43 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:07:43 up  2:02,  1 user,  load average: 1.35, 1.23, 1.11

--- npc/simulator/script/rewrite.mk
+++ npc/simulator/script/rewrite.mk
@@ -13,10 +13,17 @@ default: VysyxSoCFull
 
 include VysyxSoCFull.mk
 
-CXXFLAGS += -MMD -Ofast -g -std=c++14 -fno-exceptions -fPIE -Wall
+# DEBUG    = -g
+
+CXXFLAGS += -MMD -Ofast -std=c++14 -fno-exceptions -fPIE -Wall $(DEBUG)
+
 CXXFLAGS += $(filter-out -D__STDC_FORMAT_MACROS, $(shell llvm-config-11 --cxxflags)) \
 			-fPIC -DDEVICE -D__GUEST_ISA__=riscv64 -D__CPU_ARCH__=pipeline
+
 LDFLAGS += -Ofast -rdynamic -shared -fPIC
+
 LIBS += $(shell llvm-config-11 --libs)
+
 LIBS += -lreadline -ldl -pie -lSDL2 -lSDL2_image -lSDL2_ttf
+
 LINK := g++
