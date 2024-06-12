1c3ad83e1e65aeddc355d0f4a07a8d05aad690c1
tracer-ysyx <tracer@ysyx.org>
2024-06-12 11:16:41 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:16:41 up  2:29,  1 user,  load average: 1.81, 2.13, 1.93

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,41 +2,36 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-#define PIX_WIDTH 2
-
-volatile uint16_t x_max = 0;
-volatile uint16_t y_max = 0;
 
 void __am_gpu_init() {
 
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-
-    x_max = inw(VGACTL_ADDR + 2);
-    y_max = inw(VGACTL_ADDR);
-
     *cfg = (AM_GPU_CONFIG_T) {
         .present = true, .has_accel = false,
-        .width = x_max, .height = y_max,
-        .vmemsz = (x_max * y_max) << PIX_WIDTH
+        .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR),
+        .vmemsz = (inw(VGACTL_ADDR + 2) * inw(VGACTL_ADDR)) * sizeof(uint32_t)
     };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  register int i, j, k = 0;
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
-  if (ctl->w == 0 || ctl->h == 0) return;
-  for(j = ctl->y; j < ctl->y + ctl->h; j++) {
-    for(i = ctl->x; i < ctl->x + ctl->w; i++) {
-      if(i >= 0 && i < x_max && j >= 0 && j < y_max && ctl->pixels != NULL) {
-        outl(FB_ADDR + ((i + (j * x_max)) * sizeof(uint32_t)), *((uint32_t *)(ctl->pixels) + k));
-      }
-      k++;
+    register uint16_t x_max = inw(VGACTL_ADDR + 2);
+    register uint16_t y_max = inw(VGACTL_ADDR);
+    register int i, j, k = 0;
+
+    if (ctl->sync) {
+        outl(SYNC_ADDR, 1);
     }
-  }
+    if (ctl->w == 0 || ctl->h == 0) return;
+    for(j = ctl->y; j < ctl->y + ctl->h; j++) {
+        for(i = ctl->x; i < ctl->x + ctl->w; i++) {
+        if(i >= 0 && i < x_max && j >= 0 && j < y_max && ctl->pixels != NULL) {
+            outl(FB_ADDR + ((i + (j * x_max)) * sizeof(uint32_t)), *((uint32_t *)(ctl->pixels) + k));
+        }
+        k++;
+        }
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
