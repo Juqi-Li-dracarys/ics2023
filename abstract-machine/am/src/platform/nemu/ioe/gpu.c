33d3a83910361f483b0e700c84dadfd902e4783d
tracer-ics2023 <tracer@njuics.org>
2024-06-12 11:11:32 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:32 up  2:24,  1 user,  load average: 2.07, 2.01, 1.80

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,26 +1,30 @@
 #include <am.h>
 #include <nemu.h>
 
-
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define PIX_POWER 5
 
+volatile uint16_t x_max = 0;
+volatile uint16_t y_max = 0;
 
 void __am_gpu_init() {
 
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = inw(VGACTL_ADDR + 2), .height = inw(VGACTL_ADDR),
-    .vmemsz = inw(VGACTL_ADDR + 2) * inw(VGACTL_ADDR) * sizeof(uint32_t)
-  };
+
+    x_max = inw(VGACTL_ADDR + 2);
+    y_max = inw(VGACTL_ADDR);
+
+    *cfg = (AM_GPU_CONFIG_T) {
+        .present = true, .has_accel = false,
+        .width = x_max, .height = y_max,
+        .vmemsz = (x_max * y_max) << PIX_POWER
+    };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   register int i, j, k = 0;
-  register uint16_t x_max = inw(VGACTL_ADDR + 2);
-  register uint16_t y_max = inw(VGACTL_ADDR);
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
@@ -28,7 +32,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   for(j = ctl->y; j < ctl->y + ctl->h; j++) {
     for(i = ctl->x; i < ctl->x + ctl->w; i++) {
       if(i >= 0 && i < x_max && j >= 0 && j < y_max && ctl->pixels != NULL) {
-        outl(FB_ADDR + (i + j * x_max) * sizeof(uint32_t), *((uint32_t *)(ctl->pixels) + k));
+        outl(FB_ADDR + ((i + (j * x_max)) << PIX_POWER), *((uint32_t *)(ctl->pixels) + k));
       }
       k++;
     }
