123ae4c394cb2d8bb81c79403f4cabd27a53a997
tracer-ics2023 <tracer@njuics.org>
2024-06-12 10:33:34 +0800

    > RUN NPC 211870293 李居奇 Linux dracacys-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:33:34 up  1:46,  1 user,  load average: 1.91, 2.04, 1.71

--- npc/simulator/sim/device/vga.cpp
+++ npc/simulator/sim/device/vga.cpp
@@ -1,8 +1,8 @@
 #include <stdbool.h>
 #include <device/map.h>
 
-#define SCREEN_W 400
-#define SCREEN_H 300
+#define SCREEN_W 40
+#define SCREEN_H 30
 
 static uint32_t screen_width() {
   return SCREEN_W;
