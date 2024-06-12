3678a11aa56e5d0cd9cf283ce09e3e9ed501772b
tracer-ysyx <tracer@ysyx.org>
2024-06-13 01:42:24 +0800

    > RUN NPC ysyx23060136 李居奇 Linux dracacys-virtual-machine 6.5.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue May  7 09:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:42:24 up  2:21,  1 user,  load average: 4.14, 2.67, 1.74

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,19 +32,17 @@ void init_proc() {
   Log("Initializing processes...");
 
   // Do naive load
-  naive_uload(NULL, "/bin/pal");
+//   naive_uload(NULL, "/bin/pal");
 
-    // Do not need PCB and context switch in Season 5
+      char *argv[2] = {"--skip", NULL};
+      char *envp[2] = {NULL};
 
-    //   char *argv[2] = {"--skip", NULL};
-    //   char *envp[2] = {NULL};
+      switch_boot_pcb();
 
-    //   switch_boot_pcb();
-
-    //   // 从内核线程开始执行
-    //   context_kload(&pcb[0], hello_fun, (void *)1L);
-    //   context_uload(&pcb[1], "/bin/pal", argv, envp);
-    //   Log("Load process done...");
+      // 从内核线程开始执行
+      context_kload(&pcb[0], hello_fun, (void *)1L);
+      context_uload(&pcb[1], "/bin/pal", argv, envp);
+      Log("Load process done...");
 }
 
 Context* schedule(Context *prev) {
--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -10,19 +10,18 @@ extern char **environ;
 char *empety[1] = {NULL}; 
 
 void call_main(uintptr_t *args) {
-//   int argc = (int)args[0];
-//   int envc = (int)args[argc + 2];
-//   char **argv = (char **)(args + 1);
+  int argc = (int)args[0];
+  int envc = (int)args[argc + 2];
+  char **argv = (char **)(args + 1);
 
 //   // Do not need this arg
 //   // char **envp = (char **)(args + argc + 3);
 
-//   environ = empety;
-//   exit(main(argc, argv, empety));
+  environ = empety;
+  exit(main(argc, argv, empety));
 
 
- 
- exit(main(0, NULL, NULL));
+//   exit(main(0, NULL, NULL));
 
   assert(0);
 }
